986b6b2543648261f66470a5c23187c4ce75d7d3
tai161292@gmail.com <tai161292@gmail.com>
2020-03-04 16:29:19 +0700

    commit

--- Infrastructure/Data/CatalogDbContext.cs
+++ Infrastructure/Data/CatalogDbContext.cs
@@ -131,6 +131,8 @@ namespace Infrastructure.Data
         public DbSet<ZaloOAConfig> ZaloOAConfigs { get; set; }
         public DbSet<IRModelField> IRModelFields { get; set; }
         public DbSet<IRProperty> IRProperties { get; set; }
+        public DbSet<PartnerMapPSIDFacebookPage> PartnerMapPSIDFacebookPages { get; set; }
+        
 
         public DbSet<StockHistory> StockHistories { get; set; }
         public DbSet<AccountInvoiceReport> AccountInvoiceReports { get; set; }
@@ -242,6 +244,7 @@ namespace Infrastructure.Data
             builder.ApplyConfiguration(new ZaloOAConfigConfiguration());
             builder.ApplyConfiguration(new IRModelFieldConfiguration());
             builder.ApplyConfiguration(new IRPropertyConfiguration());
+            builder.ApplyConfiguration(new PartnerMapPSIDFacebookPageConfiguration());
 
             //var methodInfo = typeof(DbContext).GetRuntimeMethod(nameof(DatePart), new[] { typeof(string), typeof(DateTime) });
             //builder
---
+++ Infrastructure/Data/Migrations/20200304091607_PartnerMapPSIdTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class PartnerMapPSIdTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PartnerMapPSIDFacebookPages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedById = table.Column<string>(nullable: true),
+                    WriteById = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: true),
+                    LastUpdated = table.Column<DateTime>(nullable: true),
+                    PartnerId = table.Column<Guid>(nullable: false),
+                    PageId = table.Column<string>(nullable: true),
+                    PSId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PartnerMapPSIDFacebookPages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PartnerMapPSIDFacebookPages_AspNetUsers_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PartnerMapPSIDFacebookPages_Partners_PartnerId",
+                        column: x => x.PartnerId,
+                        principalTable: "Partners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PartnerMapPSIDFacebookPages_AspNetUsers_WriteById",
+                        column: x => x.WriteById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PartnerMapPSIDFacebookPages_CreatedById",
+                table: "PartnerMapPSIDFacebookPages",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PartnerMapPSIDFacebookPages_PartnerId",
+                table: "PartnerMapPSIDFacebookPages",
+                column: "PartnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PartnerMapPSIDFacebookPages_WriteById",
+                table: "PartnerMapPSIDFacebookPages",
+                column: "WriteById");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PartnerMapPSIDFacebookPages");
+        }
+    }
+}
