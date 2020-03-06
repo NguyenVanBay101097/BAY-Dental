0488e3f338f4ce54e4c4548e0ba333b014b97167
vuonghuuthien <vuonghuuthientk@gmail.com>
2020-03-06 09:07:50 +0700

    cm

--- TMTDentalAPI/ClientApp/src/app/socials-channel/socials-channel.module.ts
+++ TMTDentalAPI/ClientApp/src/app/socials-channel/socials-channel.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { SocialsChannelRoutingModule } from './socials-channel-routing.module';
 import { FacebookComponent } from './facebook/facebook.component';
 import { FacebookDialogComponent } from './facebook-dialog/facebook-dialog.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [FacebookComponent, FacebookDialogComponent],
   imports: [
     CommonModule,
-    SocialsChannelRoutingModule
+    SocialsChannelRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   exports: [
   ],
