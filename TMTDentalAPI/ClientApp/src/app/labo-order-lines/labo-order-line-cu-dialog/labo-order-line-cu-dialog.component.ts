264a05082353ba354acb72ec7e4ea694e66cebfd
ThuongHuynh1702 <titeno1231@gmail.com>
2020-06-04 17:52:34 +0700

    commit

--- TMTDentalAPI/ClientApp/src/app/labo-order-lines/labo-order-line-cu-dialog/labo-order-line-cu-dialog.component.ts
+++ TMTDentalAPI/ClientApp/src/app/labo-order-lines/labo-order-line-cu-dialog/labo-order-line-cu-dialog.component.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { LaboOrderLineService, LaboOrderLineDefaultGet, LaboOrderLineOnChangeProduct } from '../labo-order-line.service';
-import * as _ from 'lodash';
-import { IntlService } from '@progress/kendo-angular-intl';
-import { PartnerSimple, PartnerPaged } from 'src/app/partners/partner-simple';
-import { ProductSimple } from 'src/app/products/product-simple';
-import { ComboBoxComponent } from '@progress/kendo-angular-dropdowns';
-import { PartnerFilter, PartnerService } from 'src/app/partners/partner.service';
-import { ProductService, ProductFilter, ProductPaged } from 'src/app/products/product.service';
-import { debounceTime, tap, switchMap } from 'rxjs/operators';
-import { WindowRef } from '@progress/kendo-angular-dialog';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import {
+  LaboOrderLineService,
+  LaboOrderLineDefaultGet,
+  LaboOrderLineOnChangeProduct,
+} from "../labo-order-line.service";
+import * as _ from "lodash";
+import { IntlService } from "@progress/kendo-angular-intl";
+import { PartnerSimple, PartnerPaged } from "src/app/partners/partner-simple";
+import { ProductSimple } from "src/app/products/product-simple";
+import { ComboBoxComponent } from "@progress/kendo-angular-dropdowns";
+import {
+  PartnerFilter,
+  PartnerService,
+} from "src/app/partners/partner.service";
+import {
+  ProductService,
+  ProductFilter,
+  ProductPaged,
+} from "src/app/products/product.service";
+import { debounceTime, tap, switchMap } from "rxjs/operators";
+import { WindowRef } from "@progress/kendo-angular-dialog";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-labo-order-line-cu-dialog',
-  templateUrl: './labo-order-line-cu-dialog.component.html',
-  styleUrls: ['./labo-order-line-cu-dialog.component.css']
+  selector: "app-labo-order-line-cu-dialog",
+  templateUrl: "./labo-order-line-cu-dialog.component.html",
+  styleUrls: ["./labo-order-line-cu-dialog.component.css"],
 })
 export class LaboOrderLineCuDialogComponent implements OnInit {
   lineForm: FormGroup;
   id: string;
+  submitted = false;
   invoiceId: string;
   dotKhamId: string;
   filteredCustomers: PartnerSimple[] = [];
@@ -27,12 +39,18 @@ export class LaboOrderLineCuDialogComponent implements OnInit {
   filteredProducts: ProductSimple[] = [];
   title: string;
 
-  @ViewChild('customerCbx', { static: true }) customerCbx: ComboBoxComponent;
-  @ViewChild('supplierCbx', { static: true }) supplierCbx: ComboBoxComponent;
-  @ViewChild('productCbx', { static: true }) productCbx: ComboBoxComponent;
+  @ViewChild("customerCbx", { static: true }) customerCbx: ComboBoxComponent;
+  @ViewChild("supplierCbx", { static: true }) supplierCbx: ComboBoxComponent;
+  @ViewChild("productCbx", { static: true }) productCbx: ComboBoxComponent;
 
-  constructor(private fb: FormBuilder, private laboOrderLineService: LaboOrderLineService, private intlService: IntlService,
-    private partnerService: PartnerService, private productService: ProductService, public activeModal: NgbActiveModal) { }
+  constructor(
+    private fb: FormBuilder,
+    private laboOrderLineService: LaboOrderLineService,
+    private intlService: IntlService,
+    private partnerService: PartnerService,
+    private productService: ProductService,
+    public activeModal: NgbActiveModal
+  ) {}
 
   ngOnInit() {
     this.lineForm = this.fb.group({
@@ -58,35 +76,43 @@ export class LaboOrderLineCuDialogComponent implements OnInit {
       this.supplierCbx.focus();
     }, 200);
 
-
     this.loadData();
 
-    this.productCbx.filterChange.asObservable().pipe(
-      debounceTime(300),
-      tap(() => (this.productCbx.loading = true)),
-      switchMap(value => this.searchProducts(value))
-    ).subscribe(result => {
-      this.filteredProducts = result;
-      this.productCbx.loading = false;
-    });
+    this.productCbx.filterChange
+      .asObservable()
+      .pipe(
+        debounceTime(300),
+        tap(() => (this.productCbx.loading = true)),
+        switchMap((value) => this.searchProducts(value))
+      )
+      .subscribe((result) => {
+        this.filteredProducts = result;
+        this.productCbx.loading = false;
+      });
 
-    this.customerCbx.filterChange.asObservable().pipe(
-      debounceTime(300),
-      tap(() => (this.customerCbx.loading = true)),
-      switchMap(value => this.searchCustomers(value))
-    ).subscribe(result => {
-      this.filteredCustomers = result;
-      this.customerCbx.loading = false;
-    });
+    this.customerCbx.filterChange
+      .asObservable()
+      .pipe(
+        debounceTime(300),
+        tap(() => (this.customerCbx.loading = true)),
+        switchMap((value) => this.searchCustomers(value))
+      )
+      .subscribe((result) => {
+        this.filteredCustomers = result;
+        this.customerCbx.loading = false;
+      });
 
-    this.supplierCbx.filterChange.asObservable().pipe(
-      debounceTime(300),
-      tap(() => (this.supplierCbx.loading = true)),
-      switchMap(value => this.searchSuppliers(value))
-    ).subscribe(result => {
-      this.filteredSuppliers = result;
-      this.supplierCbx.loading = false;
-    });
+    this.supplierCbx.filterChange
+      .asObservable()
+      .pipe(
+        debounceTime(300),
+        tap(() => (this.supplierCbx.loading = true)),
+        switchMap((value) => this.searchSuppliers(value))
+      )
+      .subscribe((result) => {
+        this.filteredSuppliers = result;
+        this.supplierCbx.loading = false;
+      });
 
     this.loadFilteredCustomers();
     this.loadFilteredSuppliers();
@@ -94,20 +120,26 @@ export class LaboOrderLineCuDialogComponent implements OnInit {
   }
 
   loadFilteredCustomers() {
-    this.searchCustomers().subscribe(result => this.filteredCustomers = result);
+    this.searchCustomers().subscribe(
+      (result) => (this.filteredCustomers = result)
+    );
   }
 
   loadFilteredSuppliers() {
-    this.searchSuppliers().subscribe(result => this.filteredSuppliers = result);
+    this.searchSuppliers().subscribe(
+      (result) => (this.filteredSuppliers = result)
+    );
   }
 
   loadFilteredProducts() {
-    this.searchProducts().subscribe(result => this.filteredProducts = result);
+    this.searchProducts().subscribe(
+      (result) => (this.filteredProducts = result)
+    );
   }
 
   loadData() {
     if (this.id) {
-      this.laboOrderLineService.get(this.id).subscribe(result => {
+      this.laboOrderLineService.get(this.id).subscribe((result) => {
         console.log(result);
         if (result.customer) {
           this.filteredCustomers.push(result.customer);
@@ -123,24 +155,26 @@ export class LaboOrderLineCuDialogComponent implements OnInit {
 
         if (result.sentDate) {
           let sentDate = this.intlService.parseDate(result.sentDate);
-          this.lineForm.get('sentDateObj').patchValue(sentDate);
+          this.lineForm.get("sentDateObj").patchValue(sentDate);
         }
 
         if (result.receivedDate) {
           let receivedDate = this.intlService.parseDate(result.receivedDate);
-          this.lineForm.get('receivedDateObj').patchValue(receivedDate);
+          this.lineForm.get("receivedDateObj").patchValue(receivedDate);
         }
 
         if (result.warrantyPeriod) {
-          let warrantyPeriod = this.intlService.parseDate(result.warrantyPeriod);
-          this.lineForm.get('warrantyPeriodObj').patchValue(warrantyPeriod);
+          let warrantyPeriod = this.intlService.parseDate(
+            result.warrantyPeriod
+          );
+          this.lineForm.get("warrantyPeriodObj").patchValue(warrantyPeriod);
         }
       });
     } else {
       var val = new LaboOrderLineDefaultGet();
       val.invoiceId = this.invoiceId;
       val.dotKhamId = this.dotKhamId;
-      this.laboOrderLineService.defaultGet(val).subscribe(result => {
+      this.laboOrderLineService.defaultGet(val).subscribe((result) => {
         if (result.customer) {
           this.filteredCustomers.push(result.customer);
         }
@@ -148,7 +182,7 @@ export class LaboOrderLineCuDialogComponent implements OnInit {
         this.lineForm.patchValue(result);
         if (result.sentDate) {
           let sentDate = this.intlService.parseDate(result.sentDate);
-          this.lineForm.get('sentDateObj').patchValue(sentDate);
+          this.lineForm.get("sentDateObj").patchValue(sentDate);
         }
       });
     }
@@ -179,8 +213,8 @@ export class LaboOrderLineCuDialogComponent implements OnInit {
     if (value) {
       var val = new LaboOrderLineOnChangeProduct();
       val.productId = value.id;
-      this.laboOrderLineService.onChangeProduct(val).subscribe(result => {
-        this.lineForm.get('priceUnit').patchValue(result.priceUnit);
+      this.laboOrderLineService.onChangeProduct(val).subscribe((result) => {
+        this.lineForm.get("priceUnit").patchValue(result.priceUnit);
         this.computePriceSubtotal();
       });
     }
@@ -195,12 +229,13 @@ export class LaboOrderLineCuDialogComponent implements OnInit {
   }
 
   computePriceSubtotal() {
-    var quantity = this.lineForm.get('quantity').value || 0;
-    var priceUnit = this.lineForm.get('priceUnit').value || 0;
-    this.lineForm.get('priceSubtotal').patchValue(quantity * priceUnit);
+    var quantity = this.lineForm.get("quantity").value || 0;
+    var priceUnit = this.lineForm.get("priceUnit").value || 0;
+    this.lineForm.get("priceSubtotal").patchValue(quantity * priceUnit);
   }
 
   onSave() {
+    this.submitted = true;
     if (!this.lineForm.valid) {
       return;
     }
@@ -209,22 +244,29 @@ export class LaboOrderLineCuDialogComponent implements OnInit {
     val.productId = val.product.id;
     val.customerId = val.customer.id;
     val.supplierId = val.supplier.id;
-    val.sentDate = val.sentDateObj ? this.intlService.formatDate(val.sentDateObj, 'g', 'en-US') : null;
-    val.receivedDate = val.receivedDateObj ? this.intlService.formatDate(val.receivedDateObj, 'g', 'en-US') : null;
-    val.warrantyPeriod = val.warrantyPeriodObj ? this.intlService.formatDate(val.warrantyPeriodObj, 'g', 'en-US') : null;
+    val.sentDate = val.sentDateObj
+      ? this.intlService.formatDate(val.sentDateObj, "g", "en-US")
+      : null;
+    val.receivedDate = val.receivedDateObj
+      ? this.intlService.formatDate(val.receivedDateObj, "g", "en-US")
+      : null;
+    val.warrantyPeriod = val.warrantyPeriodObj
+      ? this.intlService.formatDate(val.warrantyPeriodObj, "g", "en-US")
+      : null;
 
     if (this.id) {
-      this.laboOrderLineService.update(this.id, val).subscribe(result => {
+      this.laboOrderLineService.update(this.id, val).subscribe((result) => {
         this.activeModal.close(true);
       });
     } else {
-      this.laboOrderLineService.create(val).subscribe(result => {
+      this.laboOrderLineService.create(val).subscribe((result) => {
         this.activeModal.close(result);
       });
     }
   }
 
   onCancel() {
+    this.submitted = false;
     this.activeModal.dismiss();
   }
 }
--- TMTDentalAPI/ClientApp/src/app/partners/partner-supplier-cu-dialog/partner-supplier-cu-dialog.component.ts
+++ TMTDentalAPI/ClientApp/src/app/partners/partner-supplier-cu-dialog/partner-supplier-cu-dialog.component.ts
@@ -1,39 +1,71 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { PartnerCategoryService, PartnerCategoryPaged } from 'src/app/partner-categories/partner-category.service';
-import { PartnerService } from '../partner.service';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { AppSharedShowErrorService } from 'src/app/shared/shared-show-error.service';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { FormBuilder, FormGroup, Validators, FormArray } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import {
+  PartnerCategoryService,
+  PartnerCategoryPaged,
+} from "src/app/partner-categories/partner-category.service";
+import { PartnerService } from "../partner.service";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { AppSharedShowErrorService } from "src/app/shared/shared-show-error.service";
 
 @Component({
-  selector: 'app-partner-supplier-cu-dialog',
-  templateUrl: './partner-supplier-cu-dialog.component.html',
-  styleUrls: ['./partner-supplier-cu-dialog.component.css']
+  selector: "app-partner-supplier-cu-dialog",
+  templateUrl: "./partner-supplier-cu-dialog.component.html",
+  styleUrls: ["./partner-supplier-cu-dialog.component.css"],
 })
 export class PartnerSupplierCuDialogComponent implements OnInit {
-
   id: string;
   formGroup: FormGroup;
+  submitted = false;
   isDisabledDistricts: boolean = true;
   isDisabledWards: boolean = true;
   title: string;
 
-  dataSourceCities: Array<{ code: string, name: string }>;
-  dataSourceDistricts: Array<{ code: string, name: string, cityCode: string, cityName: string }>;
-  dataSourceWards: Array<{ code: string, name: string, districtCode: string, districtName: string, cityCode: string, cityName: string }>;
+  dataSourceCities: Array<{ code: string; name: string }>;
+  dataSourceDistricts: Array<{
+    code: string;
+    name: string;
+    cityCode: string;
+    cityName: string;
+  }>;
+  dataSourceWards: Array<{
+    code: string;
+    name: string;
+    districtCode: string;
+    districtName: string;
+    cityCode: string;
+    cityName: string;
+  }>;
 
-  dataResultCities: Array<{ code: string, name: string }>;
-  dataResultDistricts: Array<{ code: string, name: string, cityCode: string, cityName: string }>;
-  dataResultWards: Array<{ code: string, name: string, districtCode: string, districtName: string, cityCode: string, cityName: string }>;
+  dataResultCities: Array<{ code: string; name: string }>;
+  dataResultDistricts: Array<{
+    code: string;
+    name: string;
+    cityCode: string;
+    cityName: string;
+  }>;
+  dataResultWards: Array<{
+    code: string;
+    name: string;
+    districtCode: string;
+    districtName: string;
+    cityCode: string;
+    cityName: string;
+  }>;
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private partnerCategoryService: PartnerCategoryService,
-    private partnerService: PartnerService, public activeModal: NgbActiveModal,
-    private showErrorService: AppSharedShowErrorService) { }
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private partnerCategoryService: PartnerCategoryService,
+    private partnerService: PartnerService,
+    public activeModal: NgbActiveModal,
+    private showErrorService: AppSharedShowErrorService
+  ) {}
 
   ngOnInit() {
     this.formGroup = this.fb.group({
-      name: ['', Validators.required],
+      name: ["", Validators.required],
       street: null,
       city: null,
       district: null,
@@ -48,7 +80,7 @@ export class PartnerSupplierCuDialogComponent implements OnInit {
 
     setTimeout(() => {
       if (this.id) {
-        this.partnerService.getPartner(this.id).subscribe(result => {
+        this.partnerService.getPartner(this.id).subscribe((result) => {
           this.formGroup.patchValue(result);
           if (result.city && result.city.code) {
             this.handleCityChange(result.city);
@@ -66,73 +98,87 @@ export class PartnerSupplierCuDialogComponent implements OnInit {
     });
   }
 
+  get f() {
+    return this.formGroup.controls;
+  }
+
   loadSourceCities() {
-    this.http.post('https://aship.skyit.vn/api/ApiShippingCity/GetCities', {
-      provider: 'Undefined'
-    }).subscribe((result: any) => {
-      this.dataSourceCities = result;
-      this.dataResultCities = this.dataSourceCities.slice();
-    });
+    this.http
+      .post("https://aship.skyit.vn/api/ApiShippingCity/GetCities", {
+        provider: "Undefined",
+      })
+      .subscribe((result: any) => {
+        this.dataSourceCities = result;
+        this.dataResultCities = this.dataSourceCities.slice();
+      });
   }
 
   loadSourceDistricts(cityCode: string) {
-    this.http.post('https://aship.skyit.vn/api/ApiShippingDistrict/GetDistricts', {
-      data: {
-        code: cityCode
-      },
-      provider: 'Undefined'
-    }).subscribe((result: any) => {
-      this.dataSourceDistricts = result;
-      this.dataResultDistricts = this.dataSourceDistricts.slice();
-    });
+    this.http
+      .post("https://aship.skyit.vn/api/ApiShippingDistrict/GetDistricts", {
+        data: {
+          code: cityCode,
+        },
+        provider: "Undefined",
+      })
+      .subscribe((result: any) => {
+        this.dataSourceDistricts = result;
+        this.dataResultDistricts = this.dataSourceDistricts.slice();
+      });
   }
 
   loadSourceWards(districtCode: string) {
-    this.http.post('https://aship.skyit.vn/api/ApiShippingWard/GetWards', {
-      data: {
-        code: districtCode
-      },
-      provider: 'Undefined'
-    }).subscribe((result: any) => {
-      this.dataSourceWards = result;
-      this.dataResultWards = this.dataSourceWards.slice();
-    });
+    this.http
+      .post("https://aship.skyit.vn/api/ApiShippingWard/GetWards", {
+        data: {
+          code: districtCode,
+        },
+        provider: "Undefined",
+      })
+      .subscribe((result: any) => {
+        this.dataSourceWards = result;
+        this.dataResultWards = this.dataSourceWards.slice();
+      });
   }
 
   handleCityFilter(value) {
-    this.dataResultCities = this.dataSourceCities.filter((s) => s.name.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+    this.dataResultCities = this.dataSourceCities.filter(
+      (s) => s.name.toLowerCase().indexOf(value.toLowerCase()) !== -1
+    );
   }
 
   handleDistrictFilter(value) {
-    this.dataResultDistricts = this.dataSourceDistricts.filter((s) => s.name.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+    this.dataResultDistricts = this.dataSourceDistricts.filter(
+      (s) => s.name.toLowerCase().indexOf(value.toLowerCase()) !== -1
+    );
   }
 
   handleWardFilter(value) {
-    this.dataResultWards = this.dataSourceWards.filter((s) => s.name.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+    this.dataResultWards = this.dataSourceWards.filter(
+      (s) => s.name.toLowerCase().indexOf(value.toLowerCase()) !== -1
+    );
   }
 
   handleCityChange(value) {
-    this.formGroup.get('city').setValue(value);
-    this.formGroup.get('district').setValue(null);
-    this.formGroup.get('ward').setValue(null);
+    this.formGroup.get("city").setValue(value);
+    this.formGroup.get("district").setValue(null);
+    this.formGroup.get("ward").setValue(null);
 
     if (value == undefined) {
       this.isDisabledDistricts = true;
       this.dataResultDistricts = [];
-    }
-    else {
+    } else {
       this.isDisabledDistricts = false;
       this.loadSourceDistricts(value.code);
     }
 
     this.isDisabledWards = true;
     this.dataResultWards = [];
-
   }
 
   handleDistrictChange(value) {
-    this.formGroup.get('district').setValue(value);
-    this.formGroup.get('ward').setValue(null);
+    this.formGroup.get("district").setValue(value);
+    this.formGroup.get("ward").setValue(null);
 
     if (value == undefined) {
       this.isDisabledWards = true;
@@ -144,29 +190,37 @@ export class PartnerSupplierCuDialogComponent implements OnInit {
   }
 
   handleWardChange(value) {
-    this.formGroup.get('ward').setValue(value);
+    this.formGroup.get("ward").setValue(value);
   }
 
   onSave() {
+    this.submitted = true;
+
     if (!this.formGroup.valid) {
       return false;
     }
 
     if (this.id) {
       var val = this.formGroup.value;
-      this.partnerService.update(this.id, val).subscribe(() => {
-        this.activeModal.close(true);
-      }, err => this.showErrorService.show(err));
+      this.partnerService.update(this.id, val).subscribe(
+        () => {
+          this.activeModal.close(true);
+        },
+        (err) => this.showErrorService.show(err)
+      );
     } else {
       var val = this.formGroup.value;
-      this.partnerService.create(val).subscribe(result => {
-        this.activeModal.close(result);
-      }, err => this.showErrorService.show(err));
+      this.partnerService.create(val).subscribe(
+        (result) => {
+          this.activeModal.close(result);
+        },
+        (err) => this.showErrorService.show(err)
+      );
     }
   }
 
   onCancel() {
+    this.submitted = false;
     this.activeModal.dismiss();
   }
 }
-
