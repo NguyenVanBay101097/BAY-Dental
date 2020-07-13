f85796ba7e346be69b2e0bb8b72d693b87deb94b
ThuongHuynh1702 <titeno1231@gmail.com>
2020-07-13 09:44:15 +0700

    cm

---
+++ TMTDentalAPI/Controllers/AccountAccountsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TMTDentalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountAccountsController : BaseApiController
+    {
+        //api get phan trang loai thu
+
+        //api get phan trang loai chi
+
+        //api get default loại thu
+
+        //api get default loai chi
+
+        //api create
+
+        //api xóa
+
+        //api update
+    }
+}
