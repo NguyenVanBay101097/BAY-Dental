dfc0bb326a0d262e7cbfec6b6309f8229e3a1005
ThuongHuynh1702 <titeno1231@gmail.com>
2020-07-15 14:02:59 +0700

    Revert "commit"
    This reverts commit 0ad9e3afd6ca63015c7659549f62f62ccbd27c66.

--- TMTDentalAPI/ClientApp/src/app/loai-thu-chi/loai-thu-chi-form/loai-thu-chi-form.component.ts
+++ TMTDentalAPI/ClientApp/src/app/loai-thu-chi/loai-thu-chi-form/loai-thu-chi-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { loaiThuChiDefault, LoaiThuChiService } from '../loai-thu-chi.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CompanyService, CompanyPaged } from 'src/app/companies/company.service';
@@ -22,8 +22,8 @@ export class LoaiThuChiFormComponent implements OnInit {
 
   ngOnInit() { 
     this.accountForm = this.fb.group({
-      name: ['', Validators.required],
-      code: ['', Validators.required],
+      name: null,
+      code: null,
       note: null,
       type: null,
       isInclude: null,
