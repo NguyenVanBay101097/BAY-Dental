54003383f8a21319ab276d05462c00cc4ffb08dc
tai161292@gmail.com <tai161292@gmail.com>
2020-08-13 17:26:22 +0700

    commit

---
+++ ApplicationCore/Entities/ResourceCalendar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class ResourceCalendar: BaseEntity
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Lấy mặc định company id của user đang đăng nhập
+        /// </summary>
+        public Guid? CompanyId { get; set; }
+        public Company Company { get; set; }
+
+        /// <summary>
+        /// Số giờ trung bình trên 1 ngày
+        /// </summary>
+        public decimal? HoursPerDay { get; set; }
+    }
+}
---
+++ Infrastructure/EntityConfigurations/ResourceCalendarConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.EntityConfigurations
+{
+    public class ResourceCalendarConfiguration : IEntityTypeConfiguration<ResourceCalendar>
+    {
+        public void Configure(EntityTypeBuilder<ResourceCalendar> builder)
+        {
+            builder.Property(x => x.Name).IsRequired();
+
+            builder.HasOne(x => x.Company)
+                .WithMany()
+                .HasForeignKey(x => x.CompanyId);
+
+            builder.HasOne(x => x.ResPartner)
+                .WithMany()
+                .HasForeignKey(x => x.ResPartnerId);
+
+            builder.HasOne(x => x.CreatedBy)
+          .WithMany()
+          .HasForeignKey(x => x.CreatedById);
+
+            builder.HasOne(x => x.WriteBy)
+                .WithMany()
+                .HasForeignKey(x => x.WriteById);
+        }
+    }
+}
