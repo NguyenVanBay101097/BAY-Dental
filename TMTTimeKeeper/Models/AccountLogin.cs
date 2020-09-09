f170323e17b5efb4fc8eeaabb1f12acdd122c260
ThuongHuynh1702 <titeno1231@gmail.com>
2020-09-09 10:14:14 +0700

    commit

--- TMTTimeKeeper/Models/AccountLogin.cs
+++ TMTTimeKeeper/Models/AccountLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,12 +7,25 @@ namespace TMTTimeKeeper.Models
 {
     public class AccountLogin
     {
+        [JsonProperty("Name")]
         public string Name { get; set; }
+
+        [JsonProperty("UserName")]
         public string UserName { get; set; }
+
+        [JsonProperty("AccessToken")]
         public string AccessToken { get; set; }
+
+        [JsonProperty("RefeshToken")]
         public string RefeshToken { get; set; }
+
+        [JsonProperty("Email")]
         public string Email { get; set; }
+
+        [JsonProperty("CompanyId")]
         public string CompanyId { get; set; }
+
+        [JsonProperty("CompanyName")]
         public string CompanyName { get; set; }
     }
 }
--- TMTTimeKeeper/Models/TimeKeeper.cs
+++ TMTTimeKeeper/Models/TimeKeeper.cs
@@ -6,6 +6,7 @@ namespace TMTTimeKeeper.Models
 {
     public class TimeKeeper
     {
+        public int Id { get; set; }
         public string CompanyName { get; set; }
         public string Name {get;set;}
         public string Seri { get; set; }
