e4a74ad08c2b0d63e3664718052e6a2c9333ca99
ThuongHuynh1702 <titeno1231@gmail.com>
2020-09-11 13:37:20 +0700

    commit

--- TMTTimeKeeper/Services/EmployeeService.cs
+++ TMTTimeKeeper/Services/EmployeeService.cs
@@ -1,18 +1,24 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TMTTimeKeeper.Info;
 using TMTTimeKeeper.Models;
+using TMTTimeKeeper.Utilities;
 
 namespace TMTTimeKeeper.Services
 {
     public class EmployeeService
     {
+        public ZkemClient objZkeeper;
+        TimeKeeperService timeKeeperObj = new TimeKeeperService();
+
         public List<EmployeeSync> getEmployee()
         {
             var employees = new List<EmployeeSync>();
@@ -57,9 +63,99 @@ namespace TMTTimeKeeper.Services
 
             var response = await HttpClientConfig.client.PostAsJsonAsync("api/Employees/SearchRead", employeePaged);
             var rs = response.Content.ReadAsStringAsync().Result;
-            List<EmployeeSync> listEmp = new List<EmployeeSync>();
             var res = JsonConvert.DeserializeObject<EmployeePagging>(rs);
             return res;
         }
+
+        public async Task<EmployeeSync> CreateEmployee(UserInfo val)
+        {
+            var empSave = new EmployeeDisplay();
+            empSave.Name = val.Name;
+            var response = await HttpClientConfig.client.PostAsJsonAsync("api/Employees/Create", empSave);
+            var rs = response.Content.ReadAsStringAsync().Result;
+            var res = JsonConvert.DeserializeObject<EmployeeDisplay>(rs);
+            var emp = new EmployeeSync();
+            if (res != null)
+            {
+                emp.Id = res.Id;
+                emp.IdKP = Int32.Parse(val.EnrollNumber);
+                emp.Name = val.Name;
+                emp.MachineNumber = val.MachineNumber;
+                emp.Password = val.Password;
+                emp.Privelage = val.Privelage;
+                emp.Enabled = val.Enabled;
+            }
+                    
+            return emp;
+        }
+
+        public ICollection<UserInfo> LoadDataEmployee()
+        {
+            objZkeeper = new ZkemClient(RaiseDeviceEvent);
+            var timeKeeperJson = timeKeeperObj.getTimekeeper();
+            var employeeJson = getEmployee();
+          
+            ICollection<UserInfo> lstFingerPrintTemplates = new Collection<UserInfo>();
+            if(timeKeeperJson != null)
+            {
+                try
+                {
+                    if (employeeJson.Count > 0)
+                    {
+                        foreach (var emp in employeeJson)
+                        {
+                            UserInfo fpInfo = new UserInfo();
+                            fpInfo.MachineNumber = emp.MachineNumber;
+                            fpInfo.EnrollNumber = emp.IdKP.ToString();
+                            fpInfo.Name = emp.Name;
+                            fpInfo.TmpData = emp.TmpData;
+                            fpInfo.Privelage = emp.Privelage;
+                            fpInfo.Password = emp.Password;
+                            fpInfo.Enabled = emp.Enabled;
+                            fpInfo.iFlag = emp.iFlag == null ? 0.ToString() : emp.iFlag;
+
+                            lstFingerPrintTemplates.Add(fpInfo);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    //DisplayListOutput(ex.Message);
+                }
+            }
+            else
+            {                
+                MessageBox.Show("Chưa kết nối máy chấm công");
+            }
+                
+          
+
+            return lstFingerPrintTemplates;
+        }
+
+        
+
+        /// <summary>
+        /// Your Events will reach here if implemented
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="actionType"></param>
+        private void RaiseDeviceEvent(object sender, string actionType)
+        {
+            switch (actionType)
+            {
+                case UniversalStatic.acx_Disconnect:
+                    {
+                        MessageBox.Show("The device is switched off");
+                        break;
+                    }
+
+                default:
+                    break;
+            }
+        }
+
+
     }
 }
