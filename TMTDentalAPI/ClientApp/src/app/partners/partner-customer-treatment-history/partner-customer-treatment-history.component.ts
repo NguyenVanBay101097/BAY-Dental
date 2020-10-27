6b483559b2f9a50d8b5d2ab8eef94fbd7b7cb8b7
vuonghuuthien <vuonghuuthientk@gmail.com>
2020-10-27 16:37:45 +0700

    This is a very important commitment

--- TMTDentalAPI/ClientApp/src/app/partners/partner-customer-treatment-history/partner-customer-treatment-history.component.ts
+++ TMTDentalAPI/ClientApp/src/app/partners/partner-customer-treatment-history/partner-customer-treatment-history.component.ts
@@ -7,6 +7,116 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartnerCustomerTreatmentHistoryComponent implements OnInit {
 
+  thTable_saleOrders = [
+    { name: 'Số phiếu' },
+    { name: 'Ngày lập phiếu' },
+    { name: 'Tổng tiền' },
+    { name: 'Còn nợ' }
+  ]
+  saleOrders: any[] = [
+    {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    },     {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    },     {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    },     {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    },     {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    },     {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    },     {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    },     {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    },     {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    },     {
+      id: "this is id", 
+      date: "this is date", 
+      money: "this is money", 
+      owe: "this is owe", 
+    }
+  ];
+
+  thTable_services = [
+    { name: 'Dịch vụ' },
+    { name: 'Đơn giá' }
+  ]
+  services: any[] = [
+    {
+      service: "this is service name", 
+      money: "this is money", 
+    },     {
+      service: "this is service name aaaaaaaaaaaaaa", 
+      money: "this is money", 
+    }
+  ];
+
+  date: Date = new Date();
+  thTable_payments = [
+    { name: 'Dịch vụ' },
+    { name: 'Răng' }, 
+    { name: 'Chuẩn đoán' }, 
+    { name: 'Số lượng' }, 
+    { name: 'Đơn giá' }, 
+    { name: 'Giảm' }, 
+    { name: 'Thành tiền' }, 
+    { name: 'Thanh toán' }, 
+    { name: 'Còn nợ' }, 
+  ]
+  payments: any[] = [
+    {
+      service: "this is service name", 
+      teeth: "this is teeth",
+      chuandoan: "this is chuandoan",
+      count: "this is count",
+      money: "this is money", 
+      discount: "this is discount", 
+      tmoney: "this is tmoney", 
+      pay: "this is pay", 
+      owe: "this is owe", 
+    }, {
+      service: "this is service name aaaaaaaaaaaaaa", 
+      teeth: "this is teeth",
+      chuandoan: "this is chuandoan",
+      count: "this is count",
+      money: "this is money", 
+      discount: "this is discount", 
+      tmoney: "this is tmoney", 
+      pay: "this is pay", 
+      owe: "this is owe", 
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
