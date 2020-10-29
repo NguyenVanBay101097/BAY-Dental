affbac01abf58931442f620136ac939ae871a197
nguyenvanbay <baynguyen101097@gmail.com>
2020-10-29 08:42:42 +0700

    cmt  format code

--- TMTDentalAPI/ClientApp/src/app/shared/services/print.service.ts
+++ TMTDentalAPI/ClientApp/src/app/shared/services/print.service.ts
@@ -3,14 +3,14 @@ declare var $: any;
 
 @Injectable({ providedIn: 'root' })
 export class PrintService {
-  public  print(html) {
-        var hiddenFrame = $('<iframe style="visibility: hidden"></iframe>').appendTo('body')[0];
-        hiddenFrame.onload = function () {
-            hiddenFrame.contentWindow.print();
-            $(hiddenFrame).remove();
-        };
-        var doc = hiddenFrame.contentWindow.document.open("text/html", "replace");
-        doc.write(html);
-        doc.close();
-      }
-    }
+  public print(html) {
+    var hiddenFrame = $('<iframe style="visibility: hidden"></iframe>').appendTo('body')[0];
+    hiddenFrame.onload = function () {
+      hiddenFrame.contentWindow.print();
+      $(hiddenFrame).remove();
+    };
+    var doc = hiddenFrame.contentWindow.document.open("text/html", "replace");
+    doc.write(html);
+    doc.close();
+  }
+}
