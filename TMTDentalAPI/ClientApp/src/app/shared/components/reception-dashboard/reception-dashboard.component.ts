57c80b0db99dda205f0bdf6ad5503a7895d807b8
vuonghuuthien <vuonghuuthientk@gmail.com>
2020-11-03 16:58:04 +0700

    Commit

--- TMTDentalAPI/ClientApp/src/app/shared/components/reception-dashboard/reception-dashboard.component.ts
+++ TMTDentalAPI/ClientApp/src/app/shared/components/reception-dashboard/reception-dashboard.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reception-dashboard',
   templateUrl: './reception-dashboard.component.html',
-  styleUrls: ['./reception-dashboard.component.css']
+  styleUrls: ['./reception-dashboard.component.css'],
+  host: {
+    class: 'o_action o_view_controller'
+  }
 })
 export class ReceptionDashboardComponent implements OnInit {
 
