6ae5e4123fd0339d60dfe87cdcabb2296d090351
tai161292@gmail.com <tai161292@gmail.com>
2020-11-16 14:57:31 +0700

    commit

--- TMTDentalAPI/Program.cs
+++ TMTDentalAPI/Program.cs
@@ -17,7 +17,7 @@ namespace TMTDentalAPI
         public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
            .SetBasePath(Directory.GetCurrentDirectory())
            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-           .AddJsonFile($"appsettings.{(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production").ToLower()}.json", optional: true)
+           .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
            .AddEnvironmentVariables()
            .Build();
 
