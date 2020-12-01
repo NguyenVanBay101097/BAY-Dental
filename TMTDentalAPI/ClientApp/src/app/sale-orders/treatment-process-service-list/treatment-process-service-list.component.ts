81020e16054de742debf695a6f0631cda98f8f8c
vuonghuuthien <vuonghuuthientk@gmail.com>
2020-12-01 16:04:41 +0700

    Commit

---
+++ TMTDentalAPI/ClientApp/src/app/sale-orders/treatment-process-service-list/treatment-process-service-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-treatment-process-service-list',
+  templateUrl: './treatment-process-service-list.component.html',
+  styleUrls: ['./treatment-process-service-list.component.css']
+})
+export class TreatmentProcessServiceListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
