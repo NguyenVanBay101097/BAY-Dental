041993c7a03cb029c50fe144d544ecfbe38ab147
nguyenvanbay <baynguyen101097@gmail.com>
2020-12-02 09:04:20 +0700

    7 cmt tam to pull code

--- TMTDentalAPI/ClientApp/src/app/sale-orders/sale-orders-routing.module.ts
+++ TMTDentalAPI/ClientApp/src/app/sale-orders/sale-orders-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SaleOrderCreateUpdateComponent } from './sale-order-create-update/sale-order-create-update.component';
 import { SaleOrderListComponent } from './sale-order-list/sale-order-list.component';
+import { SaleOrdersDotkhamCuComponent } from './sale-orders-dotkham-cu/sale-orders-dotkham-cu.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'form',
     component: SaleOrderCreateUpdateComponent
+  },
+  {
+    path: 'dotkham',
+    component: SaleOrdersDotkhamCuComponent
   },
 ];
 
