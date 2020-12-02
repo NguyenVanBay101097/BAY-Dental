d77edd530bfe1557db2e7164cfc3105847a1faf9
nguyenthangdhv95 <nguyenthangdhv95@hotmail.com>
2020-12-02 17:35:48 +0700

    commit

--- Umbraco.Web/Models/ContentEditing/DotKhamStepSave.cs
+++ Umbraco.Web/Models/ContentEditing/DotKhamStepSave.cs
@@ -6,12 +6,9 @@ namespace Umbraco.Web.Models.ContentEditing
 {
     public class DotKhamStepSave
     {
-        public Guid? DotKhamId { get; set; }
-        public string State { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
         public bool IsDone { get; set; }
-        public Guid OrderLineId { get; set; }
-        public Guid OrderId { get; set; }
-        public Guid ProductId { get; set; }
-        public bool Default { get; set; }
     }
 }
---
+++ Umbraco.Web/Models/ContentEditing/SaleOrderLineDotKhamSave.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Umbraco.Web.Models.ContentEditing
+{
+    public class SaleOrderLineDotKhamSave
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<DotKhamStepSave> Steps { get; set; } = new List<DotKhamStepSave>();
+        public bool Default { get; set; }
+
+    }
+}
