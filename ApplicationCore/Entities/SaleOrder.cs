657b9e34cc8e7758401219f871866783e1b6fa96
ThuongHuynh1702 <titeno1231@gmail.com>
2020-12-08 18:20:30 +0700

    commit

--- ApplicationCore/Entities/SaleOrder.cs
+++ ApplicationCore/Entities/SaleOrder.cs
@@ -16,6 +16,7 @@ namespace ApplicationCore.Entities
             Name = "/";
             IsQuotation = false;
             InvoiceStatus = "no";
+            isFast = false;
         }
 
         public SaleOrder(SaleOrder order)
@@ -34,6 +35,7 @@ namespace ApplicationCore.Entities
             DateOrder = DateTime.Now;
             Name = "/";
             IsQuotation = false;
+            isFast = false;
         }
 
         /// <summary>
@@ -126,5 +128,11 @@ namespace ApplicationCore.Entities
         public ICollection<SaleCoupon> GeneratedCoupons { get; set; } = new List<SaleCoupon>();
 
         public ICollection<SaleOrderServiceCardCardRel> SaleOrderCardRels { get; set; } = new List<SaleOrderServiceCardCardRel>();
+
+        public bool? isFast { get; set; }
+
+        public Guid? JournalId { get; set; }
+        public AccountJournal Journal { get; set; }
+ 
     }
 }
---
+++ Infrastructure/Data/Migrations/20201208105922_thuong_AddColumnIsFastJournal_SaleOrder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class thuong_AddColumnIsFastJournal_SaleOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "JournalId",
+                table: "SaleOrders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isFast",
+                table: "SaleOrders",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleOrders_JournalId",
+                table: "SaleOrders",
+                column: "JournalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SaleOrders_AccountJournals_JournalId",
+                table: "SaleOrders",
+                column: "JournalId",
+                principalTable: "AccountJournals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SaleOrders_AccountJournals_JournalId",
+                table: "SaleOrders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SaleOrders_JournalId",
+                table: "SaleOrders");
+
+            migrationBuilder.DropColumn(
+                name: "JournalId",
+                table: "SaleOrders");
+
+            migrationBuilder.DropColumn(
+                name: "isFast",
+                table: "SaleOrders");
+        }
+    }
+}
