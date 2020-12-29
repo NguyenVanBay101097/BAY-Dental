8185e7553b22b2adeca5ac315e94839e268b72e4
nguyenvanbay <baynguyen101097@gmail.com>
2020-12-29 17:58:17 +0700

    mising code

--- TMTDentalAPI/ClientApp/src/app/shared/partner-webcam/partner-webcam.component.ts
+++ TMTDentalAPI/ClientApp/src/app/shared/partner-webcam/partner-webcam.component.ts
@@ -18,6 +18,8 @@ export class PartnerWebcamComponent implements OnInit {
 
   public ngAfterViewInit() {
     Webcam.set({
+      width: 320,
+      height: 240,
       image_format: 'jpeg',
       jpeg_quality: 90
     });
