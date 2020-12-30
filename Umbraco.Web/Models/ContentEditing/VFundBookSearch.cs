b933f20a9ad4d7d5022165d6370a4e9061b5ccc6
nguyenthangdhv95 <nguyenthangdhv95@hotmail.com>
2020-12-30 12:04:04 +0700

    commit

--- Umbraco.Web/Models/ContentEditing/VFundBookSearch.cs
+++ Umbraco.Web/Models/ContentEditing/VFundBookSearch.cs
@@ -11,10 +11,16 @@ namespace Umbraco.Web.Models.ContentEditing
             Limit = 20;
             Offset = 0;
             InitBal = false;
+            State = "all";
         }
         public bool InitBal { get; set; }
         public DateTime? DateTo { get; set; }
         public DateTime? DateFrom { get; set; }
+        /// <summary>
+        /// all : lay tat ca
+        /// posted: phieu dc xac nhan
+        /// </summary>
+        public string State { get; set; }
         public string Type { get; set; }
         public Guid? CompanyId { get; set; }
         public string Search { get; set; }
