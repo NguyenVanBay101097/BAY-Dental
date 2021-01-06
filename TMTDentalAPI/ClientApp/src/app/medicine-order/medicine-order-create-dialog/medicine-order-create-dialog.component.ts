490360b9b037d5526239a0bf366a6c61d07b5ee7
nguyenthangdhv95 <nguyenthangdhv95@hotmail.com>
2021-01-06 16:50:13 +0700

    commit

--- TMTDentalAPI/ClientApp/src/app/medicine-order/medicine-order-create-dialog/medicine-order-create-dialog.component.ts
+++ TMTDentalAPI/ClientApp/src/app/medicine-order/medicine-order-create-dialog/medicine-order-create-dialog.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ComboBoxComponent } from '@progress/kendo-angular-dropdowns';
+import { IntlService } from '@progress/kendo-angular-intl';
 import { debounceTime, switchMap, tap } from 'rxjs/operators';
 import { AccountJournalFilter, AccountJournalService, AccountJournalSimple } from 'src/app/account-journals/account-journal.service';
 import { AuthService } from 'src/app/auth/auth.service';
+import { MedicineOrderService, PrecscriptPaymentDisplay, PrecsriptionPaymentSave } from '../medicine-order.service';
 
 @Component({
   selector: 'app-medicine-order-create-dialog',
@@ -13,7 +15,8 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class MedicineOrderCreateDialogComponent implements OnInit {
   @ViewChild('journalCbx', { static: true }) journalCbx: ComboBoxComponent;
-
+  idToaThuoc: string;
+  precscriptPaymen: PrecscriptPaymentDisplay = new PrecscriptPaymentDisplay();
   filteredJournals: AccountJournalSimple[];
   formGroup: FormGroup;
   title: string;
@@ -21,14 +24,18 @@ export class MedicineOrderCreateDialogComponent implements OnInit {
     private accountJournalService: AccountJournalService,
     private fb: FormBuilder,
     private authService: AuthService,
-    private activeModal: NgbActiveModal
+    private activeModal: NgbActiveModal,
+    private medicineOrderService: MedicineOrderService,
+    private intlService: IntlService
   ) { }
 
   ngOnInit() {
     this.formGroup = this.fb.group({
       journal: [null, Validators.required],
-      datePaymentObj: new Date(),
-      note: ''
+      dateOrder: new Date(),
+      note: '',
+      amount: 0,
+      medicineOrderLines: this.fb.array([])
     });
 
     this.loadFilteredJournals();
@@ -42,7 +49,80 @@ export class MedicineOrderCreateDialogComponent implements OnInit {
       this.journalCbx.loading = false;
     });
 
+    if (this.idToaThuoc) {
+      this.getDefault();
+    }
+  }
 
+  getDefault() {
+    this.medicineOrderService.getDefault(this.idToaThuoc).subscribe(
+      result => {
+        this.precscriptPaymen = result;
+        console.log(this.precscriptPaymen);
+
+        if (this.precscriptPaymen.toaThuoc && this.precscriptPaymen.toaThuoc.lines) {
+          var control = this.formGroup.get('medicineOrderLines') as FormArray;
+          control.clear();
+          var totalAmount = 0;
+          var lines = this.precscriptPaymen.toaThuoc.lines;
+          lines.forEach(line => {
+            control.push(this.fb.group(line));
+          });
+          this.computeTotalAmount();
+        }
+      }
+    )
+  }
+
+  get medicineOrderLines() {
+    return this.formGroup.get('medicineOrderLines') as FormArray;
+  }
+
+  computeGender(gender) {
+    switch (gender) {
+      case "male":
+        return "Nam"
+      case "female":
+        return "Nữ"
+      default:
+        break;
+    }
+  }
+
+  computeUseAt(value) {
+    switch (value) {
+      case "in_meal":
+        return "Trong khi ăn";
+      case "after_meal":
+        return "Sau khi ăn";
+      case "before_meal":
+        return "Trước khi ăn";
+      case "after_wakeup":
+        return "Sau khi dậy";
+      case "before_sleep":
+        return "Trước khi đi ngủ";
+      default:
+        break;
+    }
+  }
+
+  get getAmountTotal() {
+    return this.formGroup.get('amount').value;
+  }
+
+  computeAmountOfLine(line: FormGroup) {
+    var price = line.get('product') && line.get('product').value ? line.get('product').value.listPrice : 0;
+    var quantity = line.get('quantity') ? line.get('quantity').value : 1;
+    return quantity * price;
+  }
+
+  computeTotalAmount() {
+    var lines = this.medicineOrderLines.controls;
+    var totalAmount = 0;
+    lines.forEach((line: FormGroup) => {
+      totalAmount += this.computeAmountOfLine(line);
+    });
+    this.formGroup.get('amount').patchValue(totalAmount);
   }
 
   loadFilteredJournals() {
@@ -63,14 +143,63 @@ export class MedicineOrderCreateDialogComponent implements OnInit {
   }
 
   onSave() {
+    if (this.formGroup.invalid)
+      return
+    var val = this.formGroup.value;
+    val = this.computeForm(val)
+    this.medicineOrderService.create(val).subscribe(
+      result => {
+        // đéo làm gì
+      }
+    )
+  }
 
+  computeForm(val) {
+    val.dateOrder = this.intlService.formatDate(val.dateOrder, "yyyy-MM-ddTHH:mm");
+    val.journalId = val.journal ? val.journal.id : null;
+    val.toaThuocId = this.idToaThuoc;
+    val.employeeId = this.precscriptPaymen.employeeId;
+    val.partnerId = this.precscriptPaymen.partnerId;
+    val.companyId = this.authService.userInfo.companyId;
+    if (val.medicineOrderLines) {
+      var lines = [];
+      val.medicineOrderLines.forEach(line => {
+        var model = {
+          toaThuocLineId: line.id,
+          quantity: line.quantity ? line.quantity : 1,
+          price: line.product ? line.product.listPrice : 0,
+          amountTotal: line.quantity * line.product.listPrice,
+        }
+      });
+    }
+    return val;
   }
 
   onSavePayment() {
+    if (this.formGroup.invalid)
+      return
+    var val = this.formGroup.value;
+    this.medicineOrderService.create(val).subscribe(
+      result => {
+        if (result) {
+          var ids = [];
+          ids.push(result.id)
+          this.medicineOrderService.confirmPayment(ids).subscribe(
+            () => {
 
+            }
+          )
+          // đéo làm gì
+        }
+      }
+    )
   }
 
   onSavePaymentPrint() {
+
+  }
+
+  printPayment() {
 
   }
 
--- TMTDentalAPI/ClientApp/src/app/medicine-order/medicine-order-prescription-payment-list/medicine-order-prescription-payment-list.component.ts
+++ TMTDentalAPI/ClientApp/src/app/medicine-order/medicine-order-prescription-payment-list/medicine-order-prescription-payment-list.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ComboBoxComponent } from '@progress/kendo-angular-dropdowns';
 import { GridDataResult } from '@progress/kendo-angular-grid';
+import { IntlService } from '@progress/kendo-angular-intl';
 import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
+import { MedicineOrderService, PrecscriptionPaymentPaged } from '../medicine-order.service';
 
 @Component({
   selector: 'app-medicine-order-prescription-payment-list',
@@ -11,14 +15,63 @@ export class MedicineOrderPrescriptionPaymentListComponent implements OnInit {
   gridData: GridDataResult;
   searchUpdate = new Subject<string>();
   search: string;
+  dateFrom: Date;
+  dateTo: Date;
   loading = false;
+  state: string = '';
   limit = 20;
   offset = 0;
+  states = [
+    { value: "draft", name: "Chưa thanh toán" },
+    { value: "confirmed", name: "Đã thanh toán" },
+    { value: "cancel", name: "Hủy thanh toán" }
+  ]
   public monthStart: Date = new Date(new Date(new Date().setDate(1)).toDateString());
   public monthEnd: Date = new Date(new Date(new Date().setDate(new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate())).toDateString());
-  constructor() { }
+  constructor(
+    private intlService: IntlService,
+    private medicineOrderSerive: MedicineOrderService
+  ) { }
 
   ngOnInit() {
+    this.searchUpdate.pipe(
+      debounceTime(400),
+      distinctUntilChanged())
+      .subscribe((value) => {
+        this.search = value || '';
+        this.loadDataFromApi();
+      });
+    this.loadDataFromApi();
   }
+
+
+  loadDataFromApi() {
+    this.loading = true;
+    var paged = new PrecscriptionPaymentPaged();
+    paged.limit = this.limit;
+    paged.offset = this.offset;
+    paged.search = this.search ? this.search : '';
+    paged.dateFrom = this.intlService.formatDate(this.dateFrom, "yyyy-MM-dd");
+    paged.dateTo = this.intlService.formatDate(this.dateTo, "yyyy-MM-ddT23:50");
+    paged.state = this.state;
+    this.medicineOrderSerive.getPaged(paged).pipe(
+      map(response => (<GridDataResult>{
+        data: response.items,
+        total: response.totalItems
+      }))
+    ).subscribe(res => {
+      this.gridData = res;
+      this.loading = false;
+    }, err => {
+      console.log(err);
+      this.loading = false;
+    })
+  }
+
+  stateChange(item) {
+    this.state = item.value;
+    this.loadDataFromApi();
+  }
+
 
 }
