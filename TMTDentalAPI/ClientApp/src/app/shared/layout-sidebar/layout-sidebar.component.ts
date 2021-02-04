5352ce8cedc9f6b7c374e55bfb3aaa2a0b761294
nguyenvanbay <baynguyen101097@gmail.com>
2021-02-04 13:51:09 +0700

    cmt fix bug

--- TMTDentalAPI/ClientApp/src/app/shared/layout-sidebar/layout-sidebar.component.ts
+++ TMTDentalAPI/ClientApp/src/app/shared/layout-sidebar/layout-sidebar.component.ts
@@ -42,11 +42,10 @@ export class LayoutSidebarComponent implements OnInit {
       name: 'Khảo sát đánh giá',
       icon: 'fas fa-poll',
       children: [
-        { name: 'Danh sách khảo sát', link: '/surveys', groups:'survey_Nhanvien.survey_assignment_Nhanvien,survey_Quanly.survey_assignment_Quanly' },
-        { name: 'Quản lý phần việc', link: '/surveys/manage', groups: 'survey_Quanly.survey_assignment_Quanly' },
-        { name: 'Cấu hình đánh giá', link: '/surveys/config', groups: 'survey_Quanly.survey_assignment_Quanly' },
-      ],
-      groups: 'survey_Nhanvien.survey_assignment_Nhanvien,survey_Quanly.survey_assignment_Quanly'
+        { name: 'Danh sách khảo sát', link: '/surveys' },
+        { name: 'Quản lý phần việc', link: '/surveys/manage' },
+        { name: 'Cấu hình đánh giá', link: '/surveys/config'  },
+      ]
     },
     {
       name: 'Mua hàng',
