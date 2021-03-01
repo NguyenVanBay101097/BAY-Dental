9a847b5a8e1e9def10eb2b2111f5f57259f50698
vuonghuuthien <vuonghuuthientk@gmail.com>
2021-03-01 17:46:04 +0700

    Commit

---
+++ TMTDentalAPI/ClientApp/src/app/sale-orders/sale-order-product-request-list/sale-order-product-request-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sale-order-product-request-list',
+  templateUrl: './sale-order-product-request-list.component.html',
+  styleUrls: ['./sale-order-product-request-list.component.css']
+})
+export class SaleOrderProductRequestListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
