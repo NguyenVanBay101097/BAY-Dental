ceda5f13e40840bfe2718784e5a2f00b8a20b276
son-bui <buivanson9807@gmail.com>
2021-03-30 18:07:30 +0700

    cm

--- TMTDentalAPI/ClientApp/src/app/partners/quotation.service.ts
+++ TMTDentalAPI/ClientApp/src/app/partners/quotation.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PagedResult2 } from '../core/paged-result-2';
 
+// import { PagedResult2 } from '../paged-result-2';
+
+export class QuotationBasic {
+  id: string;
+  name: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class QuotationService {
-
-  constructor() { }
+  apiUrl = 'api/';
+  constructor(private http: HttpClient, @Inject('BASE_API') private baseApi: string) { }
+  getPaged(val: any): Observable<PagedResult2<QuotationBasic>> {
+    return this.http.get<PagedResult2<QuotationBasic>>(this.baseApi + this.apiUrl, { params: new HttpParams({ fromObject: val }) });
+  }
 }
