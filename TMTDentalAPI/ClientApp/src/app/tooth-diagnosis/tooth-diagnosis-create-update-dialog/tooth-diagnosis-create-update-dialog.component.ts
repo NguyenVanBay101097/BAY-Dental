17499839fd4eb8f915fb3f6e65531525756af8c3
NguyenNhatLuan <nhatluan98@gmail.com>
2021-03-31 11:28:22 +0700

    Chẩn đoán răng

--- TMTDentalAPI/ClientApp/src/app/tooth-diagnosis/tooth-diagnosis-create-update-dialog/tooth-diagnosis-create-update-dialog.component.ts
+++ TMTDentalAPI/ClientApp/src/app/tooth-diagnosis/tooth-diagnosis-create-update-dialog/tooth-diagnosis-create-update-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NotificationService } from '@progress/kendo-angular-notification';
+import { ToothDiagnosisService } from '../tooth-diagnosis.service';
 
 @Component({
   selector: 'app-tooth-diagnosis-create-update-dialog',
@@ -7,9 +11,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToothDiagnosisCreateUpdateDialogComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+  itemId: string;
+  myForm: FormGroup;
+  
+  submitted = false;
+  constructor(
+    private fb: FormBuilder, 
+    public activeModal: NgbActiveModal,
+    private toothDiagnosisService: ToothDiagnosisService,
+    private notificationService: NotificationService
+  ) { }
 
   ngOnInit() {
+    this.myForm = this.fb.group({
+      name: [null, Validators.required]
+    });
+    if (this.itemId) {
+      setTimeout(() => {
+        this.toothDiagnosisService.get(this.itemId).subscribe((result) => {
+          this.myForm.patchValue(result);
+        }, err => {
+          console.log(err);
+          this.activeModal.dismiss();
+        });
+      });
+    }
+  }
+
+  get f() {
+    return this.myForm.controls;
+  }
+
+  onSave(){
+    this.submitted = true;
+
+    if (!this.myForm.valid) {
+      return false;
+    }
+
+    var value = this.myForm.value;
+    if (!this.itemId) {
+      this.toothDiagnosisService.create(value).subscribe(result => {
+        this.submitted = false;
+        this.activeModal.close(result);
+        this.notificationService.show({
+          content: 'Lưu thành công',
+          hideAfter: 3000,
+          position: { horizontal: 'center', vertical: 'top' },
+          animation: { type: 'fade', duration: 400 },
+          type: { style: 'success', icon: true }
+        });
+      }, err => {
+        console.log(err);
+        this.submitted = false;
+      });
+    } else {
+      this.toothDiagnosisService.update(this.itemId, value).subscribe(result => {
+        this.submitted = false;
+        this.activeModal.close(result);
+        this.notificationService.show({
+          content: 'Lưu thành công',
+          hideAfter: 3000,
+          position: { horizontal: 'center', vertical: 'top' },
+          animation: { type: 'fade', duration: 400 },
+          type: { style: 'success', icon: true }
+        });
+      }, err => {
+        console.log(err);
+        this.submitted = false;
+      });
+    }
   }
 
 }
--- TMTDentalAPI/ClientApp/src/app/tooth-diagnosis/tooth-diagnosis.module.ts
+++ TMTDentalAPI/ClientApp/src/app/tooth-diagnosis/tooth-diagnosis.module.ts
@@ -6,12 +6,22 @@ import { ToothDiagnosisListComponent } from './tooth-diagnosis-list/tooth-diagno
 import { ToothDiagnosisCreateUpdateDialogComponent } from './tooth-diagnosis-create-update-dialog/tooth-diagnosis-create-update-dialog.component';
 
 import { MyCustomKendoModule } from '../shared/my-customer-kendo.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToothDiagnosisService } from './tooth-diagnosis.service';
 @NgModule({
   declarations: [ToothDiagnosisListComponent, ToothDiagnosisCreateUpdateDialogComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
     ToothDiagnosisRoutingModule,
     MyCustomKendoModule
+  ],
+  providers:[ToothDiagnosisService],
+  entryComponents: [
+    ToothDiagnosisCreateUpdateDialogComponent
   ]
 })
 export class ToothDiagnosisModule { }
