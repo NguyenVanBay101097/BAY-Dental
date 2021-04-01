8730a85679f55f6017adeb355e7985c04a89cb44
NguyenNhatLuan <nhatluan98@gmail.com>
2021-04-01 17:43:41 +0700

    commit

--- TMTDentalAPI/ClientApp/src/app/partners/partner-customer-advisory/partner-customer-advisory-list/partner-customer-advisory-list.component.ts
+++ TMTDentalAPI/ClientApp/src/app/partners/partner-customer-advisory/partner-customer-advisory-list/partner-customer-advisory-list.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
+import { IntlService } from '@progress/kendo-angular-intl';
 import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { AdvisoryPaged, AdvisoryService } from 'src/app/advisories/advisory.service';
+import { ToothDisplay, ToothFilter, ToothService } from 'src/app/teeth/tooth.service';
+import { ToothCategoryBasic, ToothCategoryService } from 'src/app/tooth-categories/tooth-category.service';
+import { ToothDiagnosisService } from 'src/app/tooth-diagnosis/tooth-diagnosis.service';
 import { PartnerCustomerAdvisoryCuDialogComponent } from '../partner-customer-advisory-cu-dialog/partner-customer-advisory-cu-dialog.component';
 
 @Component({
@@ -16,18 +24,169 @@ export class PartnerCustomerAdvisoryListComponent implements OnInit {
   public monthEnd: Date = new Date(new Date(new Date().setDate(new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate())).toDateString());
   dateFrom: Date;
   dateTo: Date;
+  hamList: { [key: string]: {} };
+  teethSelected: any[] = [];
+  filteredToothCategories: any[] = [];
+  cateId: string;
+  gridData: GridDataResult;
+  limit: number = 10;
+  skip: number = 0;
+  loading = false;
+  customerId: string;
+  
   constructor(
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private toothService: ToothService,
+    private toothCategoryService: ToothCategoryService,
+    private advisoryService: AdvisoryService,
+    private activeRoute: ActivatedRoute,
+    private intlService: IntlService
   ) { }
 
   ngOnInit() {
     this.dateFrom = this.monthStart;
     this.dateTo = this.monthEnd;
+    this.loadToothCategories();
+    setTimeout(() => {
+      console.log(this.teethSelected);
+      this.loadDefaultToothCategory().subscribe(result => {
+        this.cateId = result.id;
+        this.loadTeethMap(result);
+      })
+    }, 200);
+
+    this.activeRoute.parent.params.subscribe(
+      params => {
+        this.customerId = params.id;
+      }
+    )
+    this.searchUpdate.pipe(
+      debounceTime(400),
+      distinctUntilChanged())
+      .subscribe((value) => {
+        this.search = value || '';
+        this.skip = 0;
+        this.loadDataFromApi();
+      });
+    this.loadDataFromApi();
+    
   }
   createAdvisory(){
     const modalRef = this.modalService.open(PartnerCustomerAdvisoryCuDialogComponent, { scrollable: true, size: 'xl', windowClass: 'o_technical_modal', keyboard: false, backdrop: 'static' });
     modalRef.componentInstance.title = 'Thêm thông tin tư vấn';
+    modalRef.componentInstance.customerId = this.customerId;
     modalRef.result.then(() => {
     }, er => { })
   }
+
+  loadTeethMap(categ: ToothCategoryBasic) {
+    var val = new ToothFilter();
+    val.categoryId = categ.id;
+    return this.toothService.getAllBasic(val).subscribe(result => this.processTeeth(result));
+  }
+
+  processTeeth(teeth: ToothDisplay[]) {
+    this.hamList = {
+      '0_up': { '0_right': [], '1_left': [] },
+      '1_down': { '0_right': [], '1_left': [] }
+    };
+
+    for (var i = 0; i < teeth.length; i++) {
+      var tooth = teeth[i];
+      if (tooth.position === '1_left') {
+        this.hamList[tooth.viTriHam][tooth.position].push(tooth);
+      } else {
+        this.hamList[tooth.viTriHam][tooth.position].unshift(tooth);
+      }
+    }
+  }
+
+  isSelected(tooth: ToothDisplay) {
+    for (var i = 0; i < this.teethSelected.length; i++) {
+      if (this.teethSelected[i].id === tooth.id) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  onSelected(tooth: ToothDisplay) {
+    if (this.isSelected(tooth)) {
+      var index = this.getSelectedIndex(tooth);
+      this.teethSelected.splice(index, 1);
+    } else {
+      this.teethSelected.push(tooth);
+    }
+  }
+
+  getSelectedIndex(tooth: ToothDisplay) {
+    for (var i = 0; i < this.teethSelected.length; i++) {
+      if (this.teethSelected[i].id === tooth.id) {
+        return i;
+      }
+    }
+
+    return null;
+  }
+
+  loadDefaultToothCategory() {
+    return this.toothCategoryService.getDefaultCategory();
+  }
+
+  loadToothCategories() {
+    return this.toothCategoryService.getAll().subscribe(result => this.filteredToothCategories = result);
+  }
+
+  onChangeToothCategory(value: any) {
+    if (value.id) {
+      this.teethSelected = [];
+      this.loadTeethMap(value);
+      this.cateId = value.id;
+    }
+  }
+
+  loadDataFromApi(){
+    var val = new AdvisoryPaged();
+    this.loading = true;
+    val.limit = this.limit;
+    val.search = this.search ? this.search : "";
+    val.offset = this.skip;
+    val.dateFrom = this.dateFrom? this.intlService.formatDate(this.dateFrom,"yyyy-MM-dd") : "";
+    val.dateTo = this.dateTo? this.intlService.formatDate(this.dateTo,"yyyy-MM-dd") : "";
+    val.customerId = this.customerId;
+    val.toothIds = this.teethSelected.map(x => x.id);
+    this.advisoryService.getPaged(val).subscribe(res => {
+      this.gridData =  <GridDataResult> {
+        data: res.items,
+        total: res.totalItems
+      };
+      this.loading = false;
+    })
+  }
+
+  searchChangeDate(data){
+    this.dateTo = data.dateTo;
+    this.dateFrom = data.dateFrom;
+    this.skip = 0;
+    this.loadDataFromApi();
+  }
+
+  pageChange(event:PageChangeEvent){
+    this.skip = event.skip;
+    this.loadDataFromApi();
+  }
+
+  getTeeth(teeth){
+    return teeth.map(x => x.name).join(',');
+  }
+
+  getToothDiagnosis(toothDiagnosis){
+    return toothDiagnosis.map(x => x.name).join(',');
+  }
+
+  getProducts(products){
+    return products.map(x => x.name).join(',');
+  }
+
 }
