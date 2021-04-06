55861331b1b53c4dc7b2908b6761712359d99279
vuonghuuthien <vuonghuuthientk@gmail.com>
2021-04-06 16:28:43 +0700

    Commit

--- Umbraco.Web/Models/ContentEditing/QuotationViewModel.cs
+++ Umbraco.Web/Models/ContentEditing/QuotationViewModel.cs
@@ -175,7 +175,7 @@ namespace Umbraco.Web.Models.ContentEditing
 
     public class QuotationSimple
     {
-        public Guid Guid { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 
