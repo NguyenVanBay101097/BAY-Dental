13e432db49b1473fca396be1d513f126d5f6c0b5
dep_taidt <dep_taidt@VS80D52>
2021-04-15 12:24:07 +0700

    commit

--- TMTDentalAdmin/ClientApp/src/app/shared/sidebar/sidebar.module.ts
+++ TMTDentalAdmin/ClientApp/src/app/shared/sidebar/sidebar.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { SidebarNavComponent } from './sidebar-nav/sidebar-nav.component';
-import { SidebarNavDropdownComponent } from './sidebar-nav-dropdown/sidebar-nav-dropdown.component';
-import { SidebarNavItemsComponent } from './sidebar-nav-items/sidebar-nav-items.component';
-import { SidebarNavLinkComponent } from './sidebar-nav-link/sidebar-nav-link.component';
 
 @NgModule({
-  declarations: [SidebarComponent, SidebarNavComponent, SidebarNavDropdownComponent, SidebarNavItemsComponent, SidebarNavLinkComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
