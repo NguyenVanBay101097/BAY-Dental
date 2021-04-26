48d3d35fcb4ef237b5bab22244f695c70fa01765
son-bui <buivanson9807@gmail.com>
2021-04-26 11:24:56 +0700

    commit

--- TMTDentalAPI/ClientApp/src/app/sms/sms-template-cr-up/sms-template-cr-up.component.ts
+++ TMTDentalAPI/ClientApp/src/app/sms/sms-template-cr-up/sms-template-cr-up.component.ts
@@ -19,18 +19,18 @@ export class SmsTemplateCrUpComponent implements OnInit {
   skip: number = 0;
   textareaLimit: number = 500;
   id: string;
-  templates: any =
+  templates: any[] = [
     {
       text: null,
       templateType: 'text'
     }
-
+  ];
   constructor(
     public activeModal: NgbActiveModal,
     private fb: FormBuilder,
     private smsTemplateService: SmsTemplateService,
     private notificationService: NotificationService,
-
+    
   ) { }
 
   ngOnInit() {
@@ -64,7 +64,7 @@ export class SmsTemplateCrUpComponent implements OnInit {
     if (this.formGroup.invalid) { return false; }
     var formValue = this.formGroup.value;
     formValue.body = JSON.stringify(this.templates);
-
+    
     if (this.id) {
       this.smsTemplateService.update(this.id, formValue).subscribe(
         (res) => {
