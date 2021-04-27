85c73104b2c0db0f3ad55cc997e09dcdae4b9291
ThuongHuynh1702 <titeno1231@gmail.com>
2021-04-27 12:43:51 +0700

    commit

--- Infrastructure/Services/SaleCouponProgramService.cs
+++ Infrastructure/Services/SaleCouponProgramService.cs
@@ -324,7 +324,7 @@ namespace Infrastructure.Services
                 message.Error = "Khuyến mãi Không áp dụng cho đơn hàng";
             else if (!_FilterOnMinimumAmount(new List<SaleCouponProgram>() { self }, order).Any())
                 message.Error = $"Nên mua hàng tối thiểu {self.RuleMinimumAmount} để có thể nhận thưởng";
-            else if (!string.IsNullOrEmpty(self.PromoCode) && (order.Promotions.Any(x => x.SaleCouponProgramId == self.Id)))
+            else if (!string.IsNullOrEmpty(self.PromoCode) && (order.Promotions.Any(x => x.ProductId == self.DiscountLineProductId)))
                 message.Error = "Chương trình khuyến mãi đã được áp dụng cho đơn hàng này";
             else if (string.IsNullOrEmpty(self.PromoCode) && order.NoCodePromoPrograms.Select(x => x.Program).Contains(self))
                 message.Error = "Ưu đãi khuyến mãi đã được áp dụng cho đơn hàng này";
