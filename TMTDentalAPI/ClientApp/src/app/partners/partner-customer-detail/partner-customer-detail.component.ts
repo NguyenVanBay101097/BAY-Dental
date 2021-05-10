1d767bba1787d097b9d1085ab3ce67cbcde40935
NguyenNhatLuan <nhatluan98@gmail.com>
2021-05-10 09:57:18 +0700

    commit

--- TMTDentalAPI/ClientApp/src/app/partners/partner-customer-detail/partner-customer-detail.component.ts
+++ TMTDentalAPI/ClientApp/src/app/partners/partner-customer-detail/partner-customer-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CheckPermissionService } from 'src/app/shared/check-permission.service';
 import { PartnerService } from '../partner.service';
 
 @Component({
@@ -16,7 +17,12 @@ export class PartnerCustomerDetailComponent implements OnInit {
   id: string;
   partner: any;
 
-  constructor(private partnerService: PartnerService, private route: ActivatedRoute) { }
+  overview = false;
+  advisories = false;
+  labo_orders = false;
+  treatment = false;
+  advances = false;
+  constructor(private partnerService: PartnerService, private route: ActivatedRoute,private checkPermissionService: CheckPermissionService ) { }
 
   ngOnInit() {
     // this.id = this.route.snapshot.paramMap.get('id');
@@ -36,5 +42,13 @@ export class PartnerCustomerDetailComponent implements OnInit {
     this.partnerService.getPartner(this.id).subscribe((result) => {
       this.partner = result;
     });
+  }
+
+  checkRole(){
+    this.overview = this.checkPermissionService.check(['Basic.DotKham.Read']);
+   // this.advisories = this.checkPermissionService.check(['Basic.Partner.Create']);
+    this.labo_orders = this.checkPermissionService.check(['Labo.LaboOrder.Read']);
+    this.treatment = this.checkPermissionService.check(["Basic.SaleOrder.Read"])
+    this.advances = this.checkPermissionService.check(["Basic.PartnerAdvance.Read"]);
   }
 }
