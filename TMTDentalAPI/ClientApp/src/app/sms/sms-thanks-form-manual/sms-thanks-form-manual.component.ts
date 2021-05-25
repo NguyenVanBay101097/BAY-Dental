1b3f6f4e65b2d8addba1efd39b3512ec8b1619e4
vuonghuuthien <vuonghuuthientk@gmail.com>
2021-05-25 10:46:54 +0700

    Commit

--- TMTDentalAPI/ClientApp/src/app/sms/sms-thanks-form-manual/sms-thanks-form-manual.component.ts
+++ TMTDentalAPI/ClientApp/src/app/sms/sms-thanks-form-manual/sms-thanks-form-manual.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { GridDataResult } from '@progress/kendo-angular-grid';
+import { IntlService } from '@progress/kendo-angular-intl';
+import { NotificationService } from '@progress/kendo-angular-notification';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { AppointmentPaged } from 'src/app/appointment/appointment';
+import { AppointmentService } from 'src/app/appointment/appointment.service';
+import { SmsManualDialogComponent } from '../sms-manual-dialog/sms-manual-dialog.component';
 
 @Component({
   selector: 'app-sms-thanks-form-manual',
@@ -6,10 +16,100 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sms-thanks-form-manual.component.css']
 })
 export class SmsThanksFormManualComponent implements OnInit {
+  formGroup: FormGroup;
+  filteredTemplate: any[];
+  gridData: any;
+  skip: number = 0;
+  limit: number = 20;
+  isRowSelected: any[];
+  search: string = '';
+  selectedIds: string[] = [];
+  searchUpdate = new Subject<string>();
+  dateFrom: Date;
+  dateTo: Date;
+  public monthStart: Date = new Date(new Date(new Date().setDate(1)).toDateString());
+  public monthEnd: Date = new Date(new Date(new Date().setDate(new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate())).toDateString());
+  public yesterday: Date = new Date(new Date(new Date().setDate(new Date().getDate() - 1)).toDateString());
+  public today: Date = new Date();
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+    private notificationService: NotificationService,
+    private intlService: IntlService,
+    private thanksService: SmsThanksService
+  ) { }
 
   ngOnInit() {
+    this.dateFrom = this.yesterday;
+    this.dateTo = this.today;
+    this.loadDataFromApi();
+    this.searchUpdate.pipe(
+      debounceTime(400),
+      distinctUntilChanged())
+      .subscribe((value) => {
+        this.skip = 0;
+        this.loadDataFromApi();
+      });
   }
 
+  loadDataFromApi() {
+    var val = new AppointmentPaged();
+    val.limit = this.limit;
+    val.offset = this.skip;
+    val.search = this.search || '';
+    val.dateTimeFrom = this.intlService.formatDate(this.dateFrom, "yyyy-MM-dd");
+    val.dateTimeTo = this.intlService.formatDate(this.dateTo, "yyyy-MM-ddT23:59");
+
+    this.thanksService.getPaged(val).pipe(
+      map((response: any) =>
+      (<GridDataResult>{
+        data: response.items,
+        total: response.totalItems
+      }))
+    ).subscribe((res) => {
+      this.gridData = res;
+    }, err => {
+      console.log(err);
+    }
+    )
+  }
+
+  pageChange(event) {
+    this.skip = event.skip;
+    this.loadDataFromApi();
+  }
+
+  onSearchDateChange(data) {
+    this.dateFrom = data.dateFrom;
+    this.dateTo = data.dateTo;
+    this.skip = 0;
+    this.loadDataFromApi();
+  }
+
+  onSend() {
+    if (this.selectedIds.length == 0) {
+      this.notify("chưa chọn khách hàng", false);
+    }
+    else {
+      var modalRef = this.modalService.open(SmsManualDialogComponent, { size: "lg", windowClass: "o_technical_modal" });
+      modalRef.componentInstance.title = "Tạo tin gửi";
+      modalRef.componentInstance.ids = this.selectedIds ? this.selectedIds : [];
+      modalRef.componentInstance.isAppointmentReminder = true;
+      modalRef.result.then(
+        result => {
+
+        }
+      )
+    }
+  }
+
+  notify(title, isSuccess = true) {
+    this.notificationService.show({
+      content: title,
+      hideAfter: 3000,
+      position: { horizontal: 'center', vertical: 'top' },
+      animation: { type: 'fade', duration: 400 },
+      type: { style: isSuccess ? 'success' : 'error', icon: true },
+    });
+  }
 }
--- TMTDentalAPI/ClientApp/src/app/sms/sms-thanks-form/sms-thanks-form.component.ts
+++ TMTDentalAPI/ClientApp/src/app/sms/sms-thanks-form/sms-thanks-form.component.ts
@@ -1,30 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { SmsCampaignService } from '../sms-campaign.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { SmsCampaignService } from "../sms-campaign.service";
 
 @Component({
-  selector: 'app-sms-thanks-form',
-  templateUrl: './sms-thanks-form.component.html',
-  styleUrls: ['./sms-thanks-form.component.css']
+  selector: "app-sms-thanks-form",
+  templateUrl: "./sms-thanks-form.component.html",
+  styleUrls: ["./sms-thanks-form.component.css"],
 })
 export class SmsThanksFormComponent implements OnInit {
   campaign: any;
   constructor(
     private smsCampaignService: SmsCampaignService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.loadThanksCustomer();
   }
 
   loadThanksCustomer() {
-    this.smsCampaignService.getDefaultCampaignBirthday().subscribe(
-      result => {
-        if (result) {
-          this.campaign = result;
-        }
+    this.smsCampaignService.getDefaultThanksCustomer().subscribe((result) => {
+      if (result) {
+        this.campaign = result;
       }
-    );
+    });
   }
 }
