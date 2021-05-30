d5a2a9a4de2ef7b603114cb45121cf83177689fb
TAI <taidt@tmtsofts.com>
2021-05-31 00:51:10 +0700

    commit

---
+++ TMTDentalAPI/ViewControllers/SaleOrderPaymentController.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationCore.Constants;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMTDentalAPI.JobFilters;
+
+namespace TMTDentalAPI.ViewControllers
+{
+    public class SaleOrderPaymentController : Controller
+    {
+        private readonly ISaleOrderPaymentService _saleOrderPaymentService;
+
+        public SaleOrderPaymentController(ISaleOrderPaymentService saleOrderPaymentService)
+        {
+            _saleOrderPaymentService = saleOrderPaymentService;
+        }
+
+        [PrinterNameFilterAttribute(Name = AppConstants.SaleOrderPaymentPaperCode)]
+        public async Task<IActionResult> Print(Guid id)
+        {
+            var res = await _saleOrderPaymentService.GetPrint(id);
+            if (res == null) return NotFound();
+
+            return View(res);
+        }
+    }
+}
