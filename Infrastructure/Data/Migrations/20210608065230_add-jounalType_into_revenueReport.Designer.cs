20ac2f961be3dd1eedfa66760394508f44cd0d49
nguyenvanbay <baynguyen101097@gmail.com>
2021-06-08 15:32:24 +0700

    fix nguồn thu

--- Infrastructure/Data/Migrations/20210608065230_add-jounalType_into_revenueReport.Designer.cs
+++ Infrastructure/Data/Migrations/20210608065230_add-jounalType_into_revenueReport.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(CatalogDbContext))]
-    partial class CatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210608065230_add-jounalType_into_revenueReport")]
+    partial class addjounalType_into_revenueReport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
