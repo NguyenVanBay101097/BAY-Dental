d059097d263a070e08e3cc3790e68fbc78f172b9
TAI <taidt@tmtsofts.com>
2021-07-30 15:34:32 +0700

    commit

---
+++ Infrastructure/Services/FptAuthConfig.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Services
+{
+    public class FptAuthConfig
+    {
+        private string _clientId;
+        private string _clientSecret;
+        private string[] _scopes;
+        public FptAuthConfig(string clientId, string clientSecret, string[] scopes)
+        {
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _scopes = scopes;
+        }
+
+        public object GetAuthData()
+        {
+            var sessionId = StringUtils.RandomString(32);
+            return new
+            {
+                client_id = _clientId,
+                client_secret = _clientSecret,
+                scope = string.Join(" ", _scopes),
+                session_id = sessionId,
+                grant_type = "client_credentials"
+            };
+        }
+    }
+}
