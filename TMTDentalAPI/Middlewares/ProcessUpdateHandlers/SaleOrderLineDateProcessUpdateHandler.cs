156176fab387d8f35059df1a44365ea34e50c738
TAI <taidt@tmtsofts.com>
2021-09-05 21:48:57 +0700

    commit

--- TMTDentalAPI/Middlewares/ProcessUpdateHandlers/SaleOrderLineDateProcessUpdateHandler.cs
+++ TMTDentalAPI/Middlewares/ProcessUpdateHandlers/SaleOrderLineDateProcessUpdateHandler.cs
@@ -2,9 +2,12 @@
 using Dapper;
 using Infrastructure.Data;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading;
@@ -16,9 +19,12 @@ namespace TMTDentalAPI.Middlewares.ProcessUpdateHandlers
     {
         private const string _version = "1.0.1.9";
         private IServiceScopeFactory _serviceScopeFactory;
-        public SaleOrderLineDateProcessUpdateHandler(IServiceScopeFactory serviceScopeFactory)
+        private readonly ConnectionStrings _connectionStrings;
+
+        public SaleOrderLineDateProcessUpdateHandler(IServiceScopeFactory serviceScopeFactory, IOptions<ConnectionStrings> connectionStrings)
         {
             _serviceScopeFactory = serviceScopeFactory;
+            _connectionStrings = connectionStrings?.Value;
         }
 
         public Task Handle(ProcessUpdateNotification notification, CancellationToken cancellationToken)
@@ -35,10 +41,7 @@ namespace TMTDentalAPI.Middlewares.ProcessUpdateHandlers
                 if (version2.CompareTo(version1) >= 0)
                     return Task.CompletedTask;
 
-                var scopedServices = scope.ServiceProvider;
-                var context = scope.ServiceProvider.GetService<CatalogDbContext>();
-                var connectionStrings = scope.ServiceProvider.GetService<ConnectionStrings>();
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionStrings.CatalogConnection);
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(_connectionStrings.CatalogConnection);
                 builder["Database"] = $"TMTDentalCatalogDb__{tenant.Hostname}";
 
                 using (var conn = new SqlConnection(builder.ConnectionString))
@@ -46,25 +49,23 @@ namespace TMTDentalAPI.Middlewares.ProcessUpdateHandlers
                     try
                     {
                         conn.Open();
+                  
+                        var lines = conn.Query<SaleOrderLine>("select * from SaleOrderLines where Date is null");
+                        var orderIds = lines.Select(x => x.OrderId).Distinct().ToList();
+                        var orders = conn.Query<SaleOrder>("select * from SaleOrders where id in @ids", new { ids = orderIds.ToArray() });
+                        var orderDict = orders.ToDictionary(x => x.Id, x => x);
 
-                       
+                        var affectedRows = conn.Execute("UPDATE SaleOrderLines SET Date = @Date WHERE Id = @Id;",
+                          lines.Select(x => new {
+                              Id = x.Id,
+                              Date = orderDict[x.OrderId].DateOrder
+                          }).ToArray());
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine(e);
                     }
                 }
-
-
-                var orderLines = context.SaleOrderLines.Where(x => !x.Date.HasValue)
-                    .ToList();
-                foreach (var line in orderLines)
-                {
-                    line.Date = line.DateCreated;
-                }
-
-                context.SaveChanges();
-              
             }
 
             return Task.CompletedTask;
