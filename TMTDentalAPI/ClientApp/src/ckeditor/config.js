0b292650886110d3e0975bf6b226ed0e68fdb036
nguyenvanbay <supermenbay@gmail.com>
2021-09-27 16:31:07 +0700

    fix something

--- TMTDentalAPI/ClientApp/src/ckeditor/config.js
+++ TMTDentalAPI/ClientApp/src/ckeditor/config.js
@@ -7,16 +7,18 @@ CKEDITOR.editorConfig = function (config) {
 	// Define changes to default configuration here. For example:
 	config.language = 'vi';
 	config.defaultLanguage = 'vi',
-		config.enterMode = CKEDITOR.ENTER_BR;
-	// config.uiColor = '#AADC6E';
-	config.entities = false;
-	config.toolbar = [
-		{ name: 'document', items: ['Source'] },
-		{ name: 'styles', items: ['Format', 'Font', 'FontSize'] },
-		{ name: 'colors', items: ['TextColor', 'BGColor'] },
-		{ name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'Smiley'] },
-		{ name: 'basicstyles', items: ['Bold', 'Italic', 'Underline'] },
-		{ name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] }
-	]
-
+		// config.enterMode = CKEDITOR.ENTER_BR;
+		// config.uiColor = '#AADC6E';
+		config.entities = false;
+	config.allowedContent = true;
+	config.removePlugins = 'divarea',
+		config.toolbar = [
+			{ name: 'document', items: ['Source'] },
+			{ name: 'styles', items: ['Format', 'Font', 'FontSize'] },
+			{ name: 'colors', items: ['TextColor', 'BGColor'] },
+			{ name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'Smiley'] },
+			{ name: 'basicstyles', items: ['Bold', 'Italic', 'Underline'] },
+			{ name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] }
+		]
+	config.contentsCss = ['/css/print.css', CKEDITOR.basePath + 'content.config.css'];
 };
