9b9e3e2d77e0cb147ec7cc60795f9e9256579e8a
nguyenvanbay <supermenbay@gmail.com>
2021-09-29 18:11:02 +0700

    missing code

--- TMTDentalAPI/Middlewares/ProcessUpdateHandlers/IrAttachmentProcessUpdateHandler.cs
+++ TMTDentalAPI/Middlewares/ProcessUpdateHandlers/IrAttachmentProcessUpdateHandler.cs
@@ -15,7 +15,7 @@ namespace TMTDentalAPI.Middlewares.ProcessUpdateHandlers
 {
     public class IrAttachmentProcessUpdateHandler : INotificationHandler<ProcessUpdateNotification> 
     {
-        private const string _version = "1.0.1.8";
+        private const string _version = "1.0.1.9";
         private IServiceScopeFactory _serviceScopeFactory;
 
 
@@ -25,45 +25,59 @@ namespace TMTDentalAPI.Middlewares.ProcessUpdateHandlers
            
         }
 
-        public Task Handle(ProcessUpdateNotification notification, CancellationToken cancellationToken)
+        public async Task Handle(ProcessUpdateNotification notification, CancellationToken cancellationToken)
         {
             //nếu version tenant mà nhỏ hơn version app setting
             using (var scope = _serviceScopeFactory.CreateScope())
             {
                 var tenant = scope.ServiceProvider.GetService<AppTenant>();
-                if (tenant == null)
-                    return Task.CompletedTask;
-
-                Version version1 = new Version(_version);
-                Version version2 = new Version(tenant.Version);
-                if (version2.CompareTo(version1) >= 0)
-                    return Task.CompletedTask;
+                if (tenant != null)
+                {
+                    Version version1 = new Version(_version);
+                    Version version2 = new Version(tenant.Version);
+                    if (version2.CompareTo(version1) >= 0)
+                        return;
+                }
 
                 var scopedServices = scope.ServiceProvider;
                 var context = scope.ServiceProvider.GetService<CatalogDbContext>();
                 //chuyển dữ liệu bảng partnerimage => irattachment
-                var imgs = context.PartnerImages.ToList();
-                if(imgs.Any())
+                var pnImgs = context.PartnerImages.ToList();
+                if(pnImgs.Any())
                 {
                     var atts = new List<IrAttachment>();
-                    foreach (var img in imgs)
+                    var modelDataObj = scopedServices.GetService<IIRModelDataService>();
+                    var attObj = scopedServices.GetService<IIrAttachmentService>();
+                    var partnerRule = await modelDataObj.GetRef<IRRule>("base.res_partner_rule");
+                    var pnIds = pnImgs.Select(x => x.PartnerId);
+                    var dotkhamIds = pnImgs.Select(x => x.DotkhamId);
+
+                    var partners = context.Partners.Where(x => pnIds.Any(z => z == x.Id)).ToList();
+                    var dotkhams = context.DotKhams.Where(x => dotkhamIds.Any(z => z == x.Id)).ToList();
+
+                    foreach (var img in pnImgs)
                     {
-                        atts.Add( new IrAttachment()
+                        var partner = partners.FirstOrDefault(x=> x.Id == img.PartnerId);
+                        var dotkham = dotkhams.FirstOrDefault(x=> x.Id == img.DotkhamId);
+                        var companyId = img.PartnerId.HasValue ? (partnerRule.Active ? null : partner.CompanyId ) :
+                            (img.DotkhamId.HasValue ? dotkham.CompanyId : (Guid?)null) ;
+                       
+                        atts.Add(new IrAttachment()
                         {
-                            ResModel = img.PartnerId != null ? "partner" : "dot.kham",
+                            ResModel = img.PartnerId.HasValue ? "partner" : (img.DotkhamId.HasValue ? "dot.kham" : null),
                             ResId = img.PartnerId ?? img.DotkhamId,
                             Name = img.Name,
                             Type = "upload",
                             Url = img.UploadId,
-                            //CompanyId = CompanyId
+                            CompanyId = companyId
                         });
                     }
-                    context.AddRange(atts);
+                    await attObj.CreateAsync(atts);
+                    context.PartnerImages.RemoveRange(pnImgs);
                     context.SaveChanges();
                 }    
             }
 
-            return Task.CompletedTask;
         }
     }
 }
