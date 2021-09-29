a04bd01935025468c757bc1c9e4e3c907402c212
Thương Huỳnh <thuonghv@tmtsofts.com>
2021-09-29 17:41:15 +0700

    commit

--- TMTDentalAPI/ClientApp/src/app/day-dashboard-report/day-dashboard-report-cashbook/day-dashboard-report-cashbook.component.ts
+++ TMTDentalAPI/ClientApp/src/app/day-dashboard-report/day-dashboard-report-cashbook/day-dashboard-report-cashbook.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, Input, OnInit, SimpleChanges } from '@angular/core';
-import { GridDataResult } from '@progress/kendo-angular-grid';
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
 import { IntlService } from '@progress/kendo-angular-intl';
 import { forkJoin, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
@@ -13,9 +13,9 @@ import { PageGridConfig, PAGER_GRID_CONFIG } from 'src/app/shared/pager-grid-ken
   styleUrls: ['./day-dashboard-report-cashbook.component.css']
 })
 export class DayDashboardReportCashbookComponent implements OnInit {
-  @Input('dateFrom') dateFrom: any;
-  @Input('dateTo') dateTo: any;
-  @Input('company') company: any;
+  @Input() cashBookDataReport: any[] = [];
+  @Input() totalCashBook: any[] = [];
+  @Input() gridDataCashBook: any[] = [];
   gridData: GridDataResult;
   loading = false;
   skip = 0;
@@ -26,19 +26,10 @@ export class DayDashboardReportCashbookComponent implements OnInit {
     { text: 'Ngân hàng', value: 'bank' },
   ]
   search: string;
-  resultSelection = 'cash_bank';
-  summarySearchResult: any;
+  resultSelection = null;
+  summaryResult: any;
   totalCash = 0;
   totalBank = 0;
-  filterSumaryCashbookReport: any[] = [
-    { value: 'cash_bank', text: 'TM/CK', code: '131', type: 'customer' },
-    { value: 'debt', text: 'công nợ khách hàng', code: 'CNKH', type: 'customer' },
-    { value: 'advance', text: 'khách hàng tạm ứng', code: 'KHTU', type: 'customer' },
-    { value: 'cash_bank', text: 'Nhà cung cấp ', code: '331', type: 'supplier' },
-    { value: 'payroll', text: 'Chi lương và tạm ứng lương nhân viên', code: '334', type: 'customer' },
-    { value: 'commission', text: 'Hoa hồng', code: 'HHNGT', type: 'agent' },
-    { value: 'all', text: 'Total' },
-  ];
   dataCashBooks: any[];
 
   cashbookCashBank: any;
@@ -58,171 +49,79 @@ export class DayDashboardReportCashbookComponent implements OnInit {
   ) { this.pagerSettings = config.pagerSettings }
 
   ngOnInit() {
+    this.loadSumaryResult();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.loadDataFromApi();
-    this.loadGridData();
-    this.loadCashBankTotal();
-    this.loadDataCashbookApi();
-    this.loadTotalDataFromApi();
-
+    this.loadData();
+    this.loadDataCashbookReport();
+    this.loadSumaryResult();
   }
 
-  loadCashBankTotal() {
-    const companyId = this.company ? this.company.id : '';
-    const dateFrom = this.dateFrom ? this.intlService.formatDate(this.dateFrom, "yyyy-MM-dd") : null;
-    const dateTo = this.dateTo ? this.intlService.formatDate(this.dateTo, "yyyy-MM-dd") : null;
 
-    var summaryCashSearch = new CashBookSummarySearch();
-    summaryCashSearch.resultSelection = 'cash';
-    summaryCashSearch.companyId = companyId
-    summaryCashSearch.dateFrom = dateFrom
-    summaryCashSearch.dateTo = dateTo;
-
-    var summaryBankSearch = new CashBookSummarySearch();
-    summaryBankSearch.resultSelection = 'bank';
-    summaryBankSearch.companyId = companyId
-    summaryBankSearch.dateFrom = dateFrom
-    summaryBankSearch.dateTo = dateTo;
-
-    let cash = this.cashBookService.getSumary(summaryCashSearch);
-    let bank = this.cashBookService.getSumary(summaryBankSearch);
-
-    forkJoin([cash, bank]).subscribe(results => {
-      this.totalCash = results[0].totalAmount;
-      this.totalBank = results[1].totalAmount;
-    });
-
+  loadDataCashbookReport() {
+    this.cashbookCashBank = this.cashBookDataReport[0];
+    this.cashbookCusDebt = this.cashBookDataReport[1];
+    this.cashbookCusAdvance = this.cashBookDataReport[2];
+    this.cashbookSupp = this.cashBookDataReport[3];
+    this.cashbookCusSalary = this.cashBookDataReport[4];
+    this.cashbookAgentCommission = this.cashBookDataReport[5];
   }
 
-  loadDataFromApi() {
-    this.loading = true;
-    var summarySearch = new CashBookSummarySearch();
-    summarySearch.resultSelection = this.resultSelection;
-    summarySearch.companyId = this.company ? this.company.id : '';
-    summarySearch.dateFrom = this.dateFrom ? this.intlService.formatDate(this.dateFrom, "yyyy-MM-dd") : null;
-    summarySearch.dateTo = this.dateTo ? this.intlService.formatDate(this.dateTo, "yyyy-MM-dd") : null;
-    this.cashBookService.getSumary(summarySearch)
-      .subscribe(
-        (res) => {
-          this.summarySearchResult = res;
-          this.loading = false;
-        },
-        (err) => {
-          console.log(err);
-          this.loading = false;
-        }
-      );
+  public pageChange(event: PageChangeEvent): void {
+    this.skip = event.skip;
+    this.limit = event.take;
+    this.loadData();
   }
 
-  loadDataCashbookApi() {
-    forkJoin(this.filterSumaryCashbookReport.map(x => {
-      var filter = new SumaryCashBookFilter();
-      filter.dateFrom = this.dateFrom ? this.intlService.formatDate(this.dateFrom, 'yyyy-MM-dd') : '';
-      filter.dateTo = this.dateTo ? this.intlService.formatDate(this.dateTo, 'yyyy-MM-dd') : '';
-      filter.companyId = this.company ? this.company.id : '';
-      filter.resultSelection = x.value;
-      filter.accountCode = x.code || '';
-      filter.partnerType = x.type || '';
-      return this.dashboardReportService.getSumaryRevenueReport(filter).pipe(
-        switchMap(total => of({ text: x.value, total: total }))
-      );
-    })).subscribe((result) => {
-      this.dataCashBooks = result.map(x => x.total);
-      this.loadDataCashbookSeries();
-    });
+  loadData(): void {
+    debugger
+    var res = this.resultSelection == null ? this.gridDataCashBook : this.gridDataCashBook.filter(x => x.journalType == this.resultSelection);
+    this.gridData = {
+      data: res.slice(this.skip, this.skip + this.limit),
+      total: res.length
+    };
   }
 
-  loadTotalDataFromApi() {
-    this.loading = true;
-    var summarySearch = new CashBookSummarySearch();
-    summarySearch.resultSelection = 'cash_bank';
-    summarySearch.companyId = this.company ? this.company.id : '';
-    summarySearch.dateFrom = this.dateFrom ? this.intlService.formatDate(this.dateFrom, "yyyy-MM-dd") : null;
-    summarySearch.dateTo = this.dateTo ? this.intlService.formatDate(this.dateTo, "yyyy-MM-dd") : null;
-    this.cashBookService.getSumary(summarySearch)
-      .subscribe(
-        (res) => {
-          this.totalDataCashBook = res;
-          this.totalCashbook = this.totalDataCashBook;
-          this.loading = false;
-        },
-        (err) => {
-          console.log(err);
-          this.loading = false;
-        }
-      );
-  }
-
-  loadDataCashbookSeries() {
-    this.cashbookCashBank = this.dataCashBooks[0];
-    this.cashbookCusDebt = this.dataCashBooks[1];
-    this.cashbookCusAdvance = this.dataCashBooks[2];
-    this.cashbookSupp = this.dataCashBooks[3];
-    this.cashbookCusSalary = this.dataCashBooks[4];
-    this.cashbookAgentCommission = this.dataCashBooks[5];
+  loadSumaryResult() {
+    if(this.totalCashBook.length > 0){
+      if (this.resultSelection == 'cash') {
+        this.summaryResult = this.totalCashBook[1];
+      }
+      else if (this.resultSelection == 'bank') {
+        this.summaryResult = this.totalCashBook[2];
+      } else {        
+        this.summaryResult = this.totalCashBook[0];
+      }
+    }
+   
   }
 
   get ortherThu() {
     if (this.totalCashbook && this.dataCashBooks) {
       return (this.totalCashbook.totalThu ? this.totalCashbook.totalThu : 0) - (this.dataCashBooks.reduce((total, val) => total += val.credit, 0));
     }
+
     return 0;
   }
 
   get ortherChi() {
-    if (this.totalCashbook && this.dataCashBooks) {
+    if (this.totalCashbook) {
       return (this.totalCashbook.totalChi ? this.totalCashbook.totalChi : 0) - (this.cashbookSupp.debit + this.cashbookCusAdvance.debit + this.cashbookCusSalary.debit + this.cashbookAgentCommission.debit);
     }
-    return 0;
-  }
 
-  loadGridData() {
-    this.loading = true;
-    var gridPaged = new CashBookDetailFilter();
-    gridPaged.companyId = this.company ? this.company.id : '';
-    gridPaged.resultSelection = this.resultSelection;
-    gridPaged.dateFrom = this.dateFrom ? this.intlService.formatDate(this.dateFrom, "yyyy-MM-dd") : null;
-    gridPaged.dateTo = this.dateTo ? this.intlService.formatDate(this.dateTo, "yyyy-MM-dd") : null;
-    gridPaged.offset = this.skip;
-    gridPaged.limit = this.limit;
-    gridPaged.search = this.search || '';
-    this.cashBookService.getDetails(gridPaged)
-      .pipe(
-        map((response: any) =>
-          <GridDataResult>{
-            data: response.items,
-            total: response.totalItems,
-          })
-      ).subscribe(
-        (res) => {
-          // console.log(res);
-          this.gridData = res;
-          this.loading = false;
-        },
-        (err) => {
-          this.loading = false;
-        }
-      );
-  }
-  pageChange(e) {
-    this.skip = e.skip;
-    this.limit = e.take;
-    this.loadGridData();
+    return 0;
+
   }
 
   onSelectChange(e) {
     if (e) {
       this.resultSelection = e.value;
     } else {
-      this.resultSelection = 'cash_bank';
+      this.resultSelection = null;
     }
-    this.skip = 0;
-    this.loadGridData();
-    this.loadDataFromApi();
 
-    this.loadDataCashbookApi();
-    this.loadTotalDataFromApi();
+    this.loadData();
+    this.loadSumaryResult();
   }
 }
