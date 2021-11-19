9ff25ac61cde314a3542bcc6da3b32946b56407d
son-bui <buivanson9807@gmail.com>
2021-11-19 17:19:01 +0700

    commit

--- TMTDentalAPI/ClientApp/src/app/res-insurance-reports/res-insurance-debit/res-insurance-debit.component.ts
+++ TMTDentalAPI/ClientApp/src/app/res-insurance-reports/res-insurance-debit/res-insurance-debit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ResInsuranceService } from 'src/app/res-insurance/res-insurance.service';
 import { ResInsuranceCuDialogComponent } from 'src/app/shared/res-insurance-cu-dialog/res-insurance-cu-dialog.component';
 import { NotifyService } from 'src/app/shared/services/notify.service';
 
@@ -13,21 +15,34 @@ export class ResInsuranceDebitComponent implements OnInit {
   dateTo: Date;
   monthStart: Date = new Date(new Date(new Date().setDate(1)).toDateString());
   monthEnd: Date = new Date(new Date(new Date().setDate(new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate())).toDateString());
+  insuranceInfo: any;
 
   constructor(
     private modalService: NgbModal,
     private notifyService: NotifyService,
+    private activeRoute: ActivatedRoute,
+    private resInsuranceService: ResInsuranceService
   ) { }
 
   ngOnInit(): void {
     this.dateFrom = this.monthStart;
     this.dateTo = this.monthEnd;
+    this.activeRoute.parent.params.subscribe(params => {
+      const id = params.id;
+      this.getDisplayInsurance(id);
+    });
+  }
 
+  getDisplayInsurance(id: string): void {
+    this.resInsuranceService.getById(id).subscribe((res: any) => {
+      this.insuranceInfo = res;
+    })
   }
 
   editInsurance(): void {
     let modalRef = this.modalService.open(ResInsuranceCuDialogComponent, { size: 'xl', windowClass: 'o_technical_modal' });
     modalRef.componentInstance.title = 'Sửa công ty bảo hiểm';
+    modalRef.componentInstance.id = this.insuranceInfo ? this.insuranceInfo.id : '';
     modalRef.result.then(() => {
       this.notifyService.notify("success", "Lưu thành công")
     }, () => { });
