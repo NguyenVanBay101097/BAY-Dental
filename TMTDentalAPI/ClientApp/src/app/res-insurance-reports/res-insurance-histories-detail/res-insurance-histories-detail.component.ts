65490abb73758b25eac07bb9402eae9d097b4505
son-bui <buivanson9807@gmail.com>
2021-11-25 08:31:41 +0700

    fix paging

--- TMTDentalAPI/ClientApp/src/app/res-insurance-reports/res-insurance-histories-detail/res-insurance-histories-detail.component.ts
+++ TMTDentalAPI/ClientApp/src/app/res-insurance-reports/res-insurance-histories-detail/res-insurance-histories-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { GridDataResult } from '@progress/kendo-angular-grid';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
 import * as moment from 'moment';
 import { map } from 'rxjs/operators';
+import { PageGridConfig, PAGER_GRID_CONFIG } from 'src/app/shared/pager-grid-kendo.config';
 import { InsuranceHistoryInComeDetailFilter, InsuranceHistoryInComeDetailItem } from '../res-insurance-report.model';
 import { ResInsuranceReportService } from '../res-insurance-report.service';
 
@@ -15,10 +16,16 @@ export class ResInsuranceHistoriesDetailComponent implements OnInit {
   @Input() dateFrom: string;
   @Input() dateTo: string;
   gridData: GridDataResult;
+  limit: number = 20;
+  skip: number = 0;
   items: InsuranceHistoryInComeDetailItem[];
+  pagerSettings: any;
   constructor(
-    private resInsuranceReportService: ResInsuranceReportService
-  ) { }
+    private resInsuranceReportService: ResInsuranceReportService,
+    @Inject(PAGER_GRID_CONFIG) config: PageGridConfig
+  ) {
+    this.pagerSettings = config.pagerSettings
+  }
 
   ngOnInit(): void {
     this.loadDataFromApi();
@@ -37,9 +44,14 @@ export class ResInsuranceHistoriesDetailComponent implements OnInit {
 
   loadItems(): void {
     this.gridData = {
-      data: this.items,
+      data: this.items.slice(this.skip, this.skip + this.limit),
       total: this.items.length
     };
   }
 
+  public pageChange(event: PageChangeEvent): void {
+    this.skip = event.skip;
+    this.limit = event.take;
+    this.loadItems();
+  }
 }
